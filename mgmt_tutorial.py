fd9e90d2238fb2cbe62934fdd640e1a1d42da0a6
Clayton Ho <claytonho@g.ucla.edu>
2021-07-11 14:16:02 -0700

    test again

--- mgmt_tutorial.py
+++ mgmt_tutorial.py
@@ -1,16 +1,17 @@
 from artiq.experiment import *
 import numpy as np
-#import time
+import time
 
 class MgmtTutorial(EnvExperiment):
     """Management Tutorial"""
     def build(self):
         #pass #nothing
-        self.setattr_argument("count", NumberValue(ndecimals=0,step=1))
+        self.setattr_argument("count", NumberValue(ndecimals=0, step=1))
 
 
     def run(self):
-        # self.set_dataset("parabola", np.full(self.count, np.nan), broadcast=True)
+        self.set_dataset("parabola", np.full(self.count, np.nan), broadcast=True)
         for i in range(self.count):
-            print("Hello World!", i)
-        #     self.mutate_dataset("parabola", i, i * i)
+        #     print("Hello World!", i)
+            self.mutate_dataset("parabola", i, i * i)
+            time.sleep(0.5)
