e1fb931965fd3e21ee87418a9e72979e8390c86d
Clayton Ho <claytonho@g.ucla.edu>
2021-07-11 02:52:30 -0700

    First version of the tutorial experiment

---
+++ mgmt_tutorial.py
@@ -0,0 +1,10 @@
+from artiq.experiment import *
+
+class MgmtTutorial(EnvExperiment):
+    """Management Tutorial"""
+    def build(self):
+        self.setattr_argument("count", NumberValue(ndecimals=0,step=1))
+
+    def run(self):
+        for i in range(self.count):
+            print("Hello World: ", i)
