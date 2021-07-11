04f7adaa05102e325f7d4b325be58799c265c0ce
Clayton Ho <claytonho@g.ucla.edu>
2021-07-11 04:58:05 -0700

    no change?

--- mgmt_tutorial.py
+++ mgmt_tutorial.py
@@ -1,4 +1,6 @@
 from artiq.experiment import *
+import numpy as np
+import time
 
 class MgmtTutorial(EnvExperiment):
     """Management Tutorial"""
@@ -6,7 +8,9 @@ class MgmtTutorial(EnvExperiment):
         pass #nothing
         #self.setattr_argument("count", NumberValue(ndecimals=0,step=1))
 
+
     def run(self):
         print("Hello World!")
-        #for i in range(self.count):
-        #   print("Hello World: ", i)
+        # self.set_dataset("parabola", np.full(self.count, np.nan), broadcast=True)
+        # for i in range(self.count):
+        #     self.mutate_dataset("parabola", i, i * i)
