1ab2b6e3d498e014c038946fec9eb7a1e071f638
Clayton Ho <claytonho@g.ucla.edu>
2021-07-16 11:00:20 -0700

    Edit 9

--- interferometer.py
+++ interferometer.py
@@ -1,6 +1,5 @@
-from artiq.experiment import *
 import numpy as np
-#import time
+from artiq.experiment import *
 
 class Interferometer(EnvExperiment):
     """Record Interferometer Data"""
@@ -9,12 +8,10 @@ class Interferometer(EnvExperiment):
         self.setattr_argument("num_samples", NumberValue(ndecimals=0, step=1))
         self.setattr_argument("delay_time", NumberValue(ndecimals=0, step=1))
         self.setattr_argument("record_channel", NumberValue(ndecimals=0, step=1))
-
         self.setattr_device("core")
         self.setattr_device("core_dma")
         self.setattr_device("suservo0")
         self.setattr_device("suservo0_ch0")
-
         self.setattr_dataset("interferometer_data", np.full(self.num_samples, np.nan))
 
     @kernel
@@ -22,7 +19,7 @@ class Interferometer(EnvExperiment):
         with self.core_dma.record("record"):
             for i in range(self.num_samples):
                 self.mutate_dataset("interferometer_data", i, self.suservo0.get_adc(self.record_channel))
-                delay(self.delay_time*us)
+                delay(self.delay_time * us)
 
     @kernel
     def run(self):
