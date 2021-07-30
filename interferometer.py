116c4132867c8323662ce7b9d3ab4c9e2337da8f
Clayton Ho <claytonho@g.ucla.edu>
2021-07-30 14:29:54 -0700

    idk6

--- interferometer.py
+++ interferometer.py
@@ -5,21 +5,11 @@ class Interferometer(EnvExperiment):
     """Record Interferometer Data"""
 
     def build(self):
-        self.setattr_argument("num_samples", NumberValue(ndecimals=0, step=1))
-        self.setattr_argument("delay_time", NumberValue(ndecimals=0, step=1))
-        self.setattr_argument("record_channel", NumberValue(ndecimals=0, step=1))
-        self.setattr_device("core")
-        self.setattr_device("core_dma")
+        # self.setattr_argument("num_samples", NumberValue(ndecimals=0, step=1))
+        # self.setattr_argument("delay_time", NumberValue(ndecimals=0, step=1))
+        # self.setattr_argument("record_channel", NumberValue(ndecimals=0, step=1))
         self.setattr_device("sampler0")
-        self.setattr_dataset("interferometer_data", np.full(100, np.nan))
-
-    # @kernel
-    # def record(self):
-    #     with self.core_dma.record("record"):
-    #         self.sampler0.sample(self.interferometer_data)
-    #         for i in range(100):
-    #             self.mutate_dataset("interferometer_data", i, self.sampler0.sample(self.record_channel))
-    #             delay(self.delay_time * us)
+        self.setattr_dataset("interferometer_data", np.full(10, np.nan))
 
     @kernel
     def run(self):
@@ -30,11 +20,3 @@ class Interferometer(EnvExperiment):
         self.core.break_realtime()
 
         self.sampler0.sample(self.interferometer_data)
-
-        # #build record sequence
-        # self.record()
-        # record_handle = self.core_dma.get_handle("record")
-        # self.core.break_realtime()
-        #
-        # #record data
-        # self.core_dma.playback_handle(record_handle)
