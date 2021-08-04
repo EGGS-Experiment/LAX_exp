0754b4ab2342edbf2d803cabbe944fcdd2c67859
Clayton Ho <claytonho@g.ucla.edu>
2021-08-04 01:20:43 -0700

    idk6

--- interferometer.py
+++ interferometer.py
@@ -26,7 +26,7 @@ class Interferometer(EnvExperiment):
 
         for ind in range(10):
             #self.sampler0.sample_mu(holder)
-            self.mutate_dataset("interferometer_data", ind, ind^2)
+            self.mutate_dataset("interferometer_data", ind, ind**2)
             delay(10*us)
 
         print("done")
