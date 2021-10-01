38e2a19f8e1e8db36d5c55a2f7c94af714b34460
Clayton Ho <claytonho@g.ucla.edu>
2021-09-30 19:36:52 -0700

    th1

--- mgmt_tutorial.py
+++ mgmt_tutorial.py
@@ -8,9 +8,9 @@ class MgmtTutorial(EnvExperiment):
         #pass #nothing
         self.setattr_argument("count", NumberValue(ndecimals=0, step=1))
 
-    def run(self):
-        self.set_dataset("parabola", np.full(self.count, np.nan), broadcast=True)
-        for i in range(self.count):
-        #     print("Hello World!", i)
-            self.mutate_dataset("parabola", i, i * i)
-            time.sleep(0.5)
+    # def run(self):
+    #     self.set_dataset("parabola", np.full(self.count, np.nan), broadcast=True)
+    #     for i in range(self.count):
+    #     #     print("Hello World!", i)
+    #         self.mutate_dataset("parabola", i, i * i)
+    #         time.sleep(0.5)
