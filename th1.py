a8623c301a305e14d6cc32b830ea473150f80e82
Clayton Ho <claytonho@g.ucla.edu>
2021-10-15 02:45:28 -0700

    tmp

--- th1.py
+++ th1.py
@@ -1,6 +1,36 @@
+"""
+### BEGIN NODE INFO
+[info]
+name = yzd1
+version = 1.0.0
+description = Talks to the Lakeshore 336 Temperature Controller
+instancename = yzd1
+
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+
+[shutdown]
+message = 987654321
+timeout = 20
+### END NODE INFO
+"""
+
+SERVERNAME = 'lakeshore336Server'
+TIMEOUT = 1.0
+BAUDRATE = 57600
+BYTESIZE = 7
+STOPBITS = 1
+INPUT_CHANNELS = ['A', 'B', 'C', 'D', '0']
+OUTPUT_CHANNELS = [1, 2, 3, 4]
+TERMINATOR = '\r\n'
+
 from artiq.experiment import *
 import numpy as np
-import time
+
+from labrad.support import getNodeName
+from labrad.devices import LabradServer
+from labrad.server import setting
 
 from serial import Serial, PARITY_ODD
 from sipyco.pc_rpc import simple_server_loop
@@ -29,10 +59,69 @@ class Lakeshore336(object):
     def close(self):
         self.ser.close()
 
+class Lakeshore3362(HasEnvironment, LabradServer):
+    name = "lakeshore2"
+    regKey = "lakeshore2"
+    serNode = getNodeName()
+
+    def build(self):
+        print("thkimHE")
+
+    # TEMPERATURE DIODES
+    @setting(111,'Read Temperature')
+    def temperature_read(self, c):
+        """
+        Get sensor temperature
+        """
+        print("thkim1")
+        #self.setattr_device("ttl7")
+        print('thkim2')
+        #self.get_device_db()
+
 class TH1(EnvExperiment):
-    """Management Tutorial"""
+    """TH1"""
+    # def run(self):
+    #     simple_server_loop({"Lakeshore336": Lakeshore336}, "localhost", 7921)
+    def build(self):
+        print(self.get_device_db())
+
     def run(self):
-        try:
-            simple_server_loop({"Lakeshore336": Lakeshore336}, "::1", 3259)
-        finally:
-            dev.close()
+        from labrad import util
+        #util.runServer(Lakeshore3362(self))
+        util.runServer(yzd1(self))
+
+    def okth1(self):
+        print("this works ok")
+
+
+"""
+### BEGIN NODE INFO
+[info]
+name = yzd1
+version = 1.0.0
+description = horrible
+instancename = yzd1
+
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+
+[shutdown]
+message = 987654321
+timeout = 20
+### END NODE INFO
+"""
+class yzd1(LabradServer):
+    name = "yzd1"
+    regKey = "yzd1"
+    serNode = getNodeName()
+
+    def __init__(self, ee):
+        self.ee = ee
+        LabradServer.__init__(self)
+
+    @setting(123, "thkim1")
+    def thkim1(self, c):
+        "func ok"
+        print("hate")
+        self.ee.okth1()
