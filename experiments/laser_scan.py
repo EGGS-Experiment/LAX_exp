a7cdc958e5e53a77ed244c43dc1066858e7e9410
Clayton Ho <claytonho@g.ucla.edu>
2022-07-29 15:05:38 -0700

    cleaned up laser scan experiment

--- experiments/laser_scan.py
+++ experiments/laser_scan.py
@@ -2,9 +2,7 @@ import numpy as np
 from artiq.experiment import *
 
 _DMA_HANDLE = "laser_scan"
-# todo: make repump status more general
 # todo: upload data to labrad
-# todo: check synchronization of cycle with now_mu()
 # todo: check scannable works correctly
 
 
@@ -27,17 +25,20 @@ class LaserScan(EnvExperiment):
         self.setattr_argument("repetitions",            NumberValue(default=100, ndecimals=0, step=1, min=1, max=10000))
 
         # timing
-        self.setattr_argument("time_delay_us",          NumberValue(default=100, ndecimals=2, step=1, min=1, max=1000))
-        self.setattr_argument("time_probe_us",          NumberValue(default=100, ndecimals=5, step=1, min=1, max=1000))
+        self.setattr_argument("time_qubit_us",          NumberValue(default=5000, ndecimals=5, step=1, min=1, max=10000))
 
         # AOM DDS channels
         self.setattr_argument("dds_board_num",          NumberValue(default=0, ndecimals=0, step=1, min=0, max=1))
         self.setattr_argument("dds_qubit_channel",      NumberValue(default=0, ndecimals=0, step=1, min=0, max=3))
 
-        # probe frequency scan
-        self.setattr_argument("freq_probe_scan_mhz",    Scannable(default=RangeScan(65, 85, 5),
-                                                                  global_min=60, global_max=110, global_step=1,
-                                                                  unit="MHz", scale=1, ndecimals=1))
+        # AOM DDS parameters
+        self.setattr_argument("freq_qubit_mhz",          NumberValue(default=110, ndecimals=3, step=1, min=10, max=200))
+
+
+        # qubit frequency scan
+        self.setattr_argument("freq_qubit_scan_mhz",    Scannable(default=RangeScan(100, 120, 0.005),
+                                                                  global_min=60, global_max=200, global_step=1,
+                                                                  unit="MHz", scale=1, ndecimals=3))
 
         # PMT
         self.setattr_argument("pmt_input_channel",      NumberValue(default=0, ndecimals=0, step=1, min=0, max=3))
@@ -53,24 +54,22 @@ class LaserScan(EnvExperiment):
         self.pmt_gating_edge = getattr(self.pmt_counter, 'gate_{:s}_mu'.format(self.pmt_gating_edge))
 
         # convert time values to machine units
-        self.time_probe_mu = self.core.seconds_to_mu(self.time_probe_us * us)
-        self.time_delay_mu = self.core.seconds_to_mu(self.time_delay_us * us)
+        self.time_qubit_mu = self.core.seconds_to_mu(self.time_qubit_us * us)
 
         # DDS devices
         self.dds_board = self.get_device("urukul{:d}_cpld".format(self.dds_board_num))
-        self.dds_probe = self.get_device("urukul{:d}_ch{:d}".format(self.dds_board_num, self.dds_probe_channel))
+        self.dds_qubit = self.get_device("urukul{:d}_ch{:d}".format(self.dds_board_num, self.dds_qubit_channel))
 
-        # convert dds values to machine units - probe
+        # convert dds values to machine units - qubit
         self.ftw_to_frequency = 1e9 / (2**32 - 1)
-        self.freq_probe_scan_mhz2 = list(self.freq_probe_scan_mhz)
+        self.freq_qubit_scan_mhz2 = list(self.freq_qubit_scan_mhz)
 
         # convert dds values to machine units - everything else
-        self.freq_pump_ftw = self.dds_pump.frequency_to_ftw(self.freq_pump_mhz * MHz)
-        self.freq_repump_ftw = self.dds_probe.frequency_to_ftw(self.freq_repump_mhz * MHz)
-        self.ampl_pump_asf = self.dds_pump.amplitude_to_asf(0.5)
+        self.freq_qubit_ftw = self.dds_qubit.frequency_to_ftw(self.freq_qubit_mhz * MHz)
+        self.ampl_qubit_asf = self.dds_qubit.amplitude_to_asf(0.5)
 
         # set up datasets
-        self.set_dataset("temperature_measurement", [], broadcast=True)
+        self.set_dataset("laser_scan", [], broadcast=True)
 
     @kernel(flags={"fast-math"})
     def run(self):
@@ -92,40 +91,21 @@ class LaserScan(EnvExperiment):
         self.core.break_realtime()
 
         # MAIN SEQUENCE
-        for freq_mhz in self.freq_probe_scan_mhz2:
-            self.core.break_realtime()
+        for trial_num in range(self.reptitions):
+            # scan frequency
+            for freq_mhz in self.freq_qubit_scan_mhz2:
+                self.core.break_realtime()
 
-            # set freq and ampl for probe
-            freq_mu = self.dds_probe.frequency_to_ftw(freq_mhz)
-            self.dds_probe.set_mu(freq_mu, asf=self.ampl_probe_asf)
-            self.core.break_realtime()
+                # set freq and ampl for qubit
+                freq_mu = self.dds_qubit.frequency_to_ftw(freq_mhz)
+                self.dds_qubit.set_mu(freq_mu, asf=self.ampl_qubit_asf)
+                self.core.break_realtime()
 
-            # run the experiment
-            self.dds_repump.cfg_sw(1)
-            for trial_num in range(self.repetitions):
-                # run pulse sequence from core DMA
-                self.core_dma.playback_handle(handle)
-
-                # update dataset
-                with parallel:
-                    self.update_dataset(freq_mhz, self.pmt_counter.fetch_count())
-                    self.core.break_realtime()
-                #delay_mu(self.time_delay_mu)
-
-    @kernel(flags={"fast-math"})
-    def DMArecord(self):
-        """
-        Record onto core DMA the AOM sequence for a single data point.
-        """
-        with self.core_dma.record(_DMA_HANDLE):
-            # pump on, probe off
-            with parallel:
-                self.dds_board.cfg_switches(self.dds_switch_pump_states_on)
-                delay_mu(self.time_pump_mu)
-            # probe on, pump off, PMT start recording
-            with parallel:
-                self.dds_board.cfg_switches(self.dds_switch_probe_states_on)
-                self.pmt_gating_edge(self.time_probe_mu)
+                # record fluorescence
+                self.dds_qubit.on()
+                self.pmt_gating_edge(self.time_qubit_mu)
+                self.dds_qubit.off()
+                self.update_dataset(trial_num, freq_mhz, self.pmt_counter.fetch_count())
 
     @kernel(flags={"fast-math"})
     def prepareDevices(self):
@@ -138,8 +118,8 @@ class LaserScan(EnvExperiment):
         #self.dds_board.init()
         self.core.break_realtime()
 
-        # initialize pump beam and set waveform
-        #self.dds_pump.init()
+        # initialize qubit AOM and set waveform
+        #self.dds_qubit.init()
         self.core.break_realtime()
         self.dds_qubit.set_mu(self.freq_qubit_ftw, asf=self.ampl_qubit_asf)
         self.core.break_realtime()
@@ -148,21 +128,20 @@ class LaserScan(EnvExperiment):
         self.dds_qubit.cfg_sw(1)
 
     @rpc(flags={"async"})
-    def update_dataset(self, freq_mhz, pmt_counts):
+    def update_dataset(self, trial_num, freq_mhz, pmt_counts):
         """
         Records values via rpc to minimize kernel overhead.
         """
-        self.append_to_dataset('temperature_measurement', [freq_mhz, pmt_counts])
+        self.append_to_dataset('laser_scan', [trial_num, freq_mhz, pmt_counts])
 
     def analyze(self):
         """
         Analyze the results from the experiment.
         """
         pass
-        # data = self.get_dataset("temperature_measurement_repump_on")
+        # data = self.get_dataset("laser_scan")
         # pmt_data = data[2500:, 1]
-        # pd_data = data[2500:, 2]
         #
-        # print("repump on:")
+        # print("results:")
         # print("\tpmt counts: {:.2f} +/- {:.2f}".format(np.mean(pmt_data), np.std(pmt_data)))
         # print("\tphotodiode value: {:.2f} +/- {:.2f}".format(np.mean(pd_data), np.std(pd_data)))
