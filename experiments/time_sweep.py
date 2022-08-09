ef221ee3441f835f70f0e5afb1e6c5b5e91b8f71
Clayton Ho <claytonho@g.ucla.edu>
2022-08-08 17:47:42 -0700

    updated time sweep experiment

--- experiments/time_sweep.py
+++ experiments/time_sweep.py
@@ -4,9 +4,9 @@ from artiq.experiment import *
 # todo: check scannable works correctly
 
 
-class LaserScan(EnvExperiment):
+class TimeSweep(EnvExperiment):
     """
-    729nm Laser Scan
+    729nm Laser Time Sweep
     Measures ion fluorescence vs AOM detuning
     """
 
@@ -24,15 +24,21 @@ class LaserScan(EnvExperiment):
 
         # timing
         self.setattr_argument("time_qubit_us",          NumberValue(default=5000, ndecimals=5, step=1, min=1, max=10000))
+        self.setattr_argument("time_readout_us",        NumberValue(default=5000, ndecimals=5, step=1, min=1, max=10000))
 
         # AOM DDS channels
-        self.setattr_argument("dds_board_num",          NumberValue(default=0, ndecimals=0, step=1, min=0, max=1))
+        self.setattr_argument("dds_board_num",          NumberValue(default=1, ndecimals=0, step=1, min=0, max=1))
         self.setattr_argument("dds_qubit_channel",      NumberValue(default=0, ndecimals=0, step=1, min=0, max=3))
 
         # qubit frequency scan
-        self.setattr_argument("freq_qubit_scan_mhz",    Scannable(default=RangeScan(109.4, 109.8, 401),
+        self.setattr_argument("freq_qubit_scan_mhz",    Scannable(default=RangeScan(100, 120, 5),
                                                                   global_min=60, global_max=200, global_step=1,
                                                                   unit="MHz", scale=1, ndecimals=3))
+
+        # qubit time scan
+        self.setattr_argument("time_sweep_us",          Scannable(default=RangeScan(1, 1000, 21),
+                                                                  global_min=1, global_max=10000, global_step=1,
+                                                                  unit="us", scale=1, ndecimals=0))
 
         # PMT
         self.setattr_argument("pmt_input_channel",      NumberValue(default=0, ndecimals=0, step=1, min=0, max=3))
@@ -48,7 +54,8 @@ class LaserScan(EnvExperiment):
         self.pmt_gating_edge = getattr(self.pmt_counter, 'gate_{:s}_mu'.format(self.pmt_gating_edge))
 
         # convert time values to machine units
-        self.time_qubit_mu = self.core.seconds_to_mu(self.time_qubit_us * us)
+        self.time_854_mu = self.core.seconds_to_mu(self.time_854_us * us)
+        self.time_readout_mu = self.core.seconds_to_mu(self.time_readout_us * us)
 
         # DDS devices
         self.dds_board = self.get_device("urukul{:d}_cpld".format(self.dds_board_num))
@@ -57,6 +64,9 @@ class LaserScan(EnvExperiment):
         # convert dds values to machine units - qubit
         self.ftw_to_frequency = 1e9 / (2**32 - 1)
         self.freq_qubit_scan_mhz2 = list(self.freq_qubit_scan_mhz)
+
+        # convert time values for sweep to machine values
+        self.time_sweep_mu2 = [self.core.seconds_to_mu(val * us) for val in self.time_sweep_us]
 
         # convert dds values to machine units - everything else
         self.ampl_qubit_asf = self.dds_qubit.amplitude_to_asf(0.5)
@@ -67,8 +77,6 @@ class LaserScan(EnvExperiment):
 
         # tmp remove:
         self.setattr_device('urukul1_cpld')
-        self.yzde = self.core.seconds_to_mu(500 * us)
-        #print(len(self.freq_qubit_scan_mhz2))
 
     @kernel(flags={"fast-math"})
     def run(self):
@@ -83,31 +91,46 @@ class LaserScan(EnvExperiment):
 
         # MAIN SEQUENCE
         for trial_num in range(self.repetitions):
-            # scan frequency
+
+            # set frequencies
             for freq_mhz in self.freq_qubit_scan_mhz2:
                 self.core.break_realtime()
-
-                # turn on 854 to pump back down
-                self.urukul1_cpld.cfg_switches(0b1111)
-                delay(400 * us)
-                self.urukul1_cpld.cfg_switches(0b0111)
-                self.core.break_realtime()
-
-                # get pmt counts to calibrate
-                self.pmt_gating_edge(self.yzde)
-                val = self.pmt_counter.fetch_count()
-                delay(1 * ms)
 
                 # set freq and ampl for qubit
                 freq_mu = self.dds_qubit.frequency_to_ftw(freq_mhz * MHz)
                 self.dds_qubit.set_mu(freq_mu, asf=self.ampl_qubit_asf)
-
-                # record fluorescence
-                self.dds_qubit.cfg_sw(1)
-                self.pmt_gating_edge(self.time_qubit_mu)
                 self.core.break_realtime()
-                self.dds_qubit.cfg_sw(0)
-                self.update_dataset(trial_num, freq_mhz, self.pmt_counter.fetch_count(), val)
+
+                # sweep time
+                for time_mu in self.time_sweep_mu2:
+
+                    # turn on 854 to pump back down
+                    with parallel:
+                        self.urukul1_cpld.cfg_switches(0b1100)
+                        delay_mu(self.time_854_mu)
+                    self.urukul1_cpld.cfg_switches(0b0100)
+
+                    # get pmt counts w/397 onto calibrate
+                    with parallel:
+                        self.urukul1_cpld.cfg_switches(0b0110)
+                        self.pmt_gating_edge(self.time_readout_mu)
+                    with parallel:
+                        pmt_calib = self.pmt_counter.fetch_count()
+                        self.urukul1_cpld.cfg_switches(0b0100)
+
+                    # rabi flopping w/qubit laser
+                    with parallel:
+                        self.dds_qubit.cfg_sw(1)
+                        self.pmt_gating_edge(time_mu)
+                    self.dds_qubit.cfg_sw(0)
+
+                    # get pmt counts (actual)
+                    with parallel:
+                        self.urukul1_cpld.cfg_switches(0b0110)
+                        self.pmt_gating_edge(self.time_readout_mu)
+                    self.urukul1_cpld.cfg_switches(0b0100)
+
+                    self.update_dataset(time_mu, freq_mhz, self.pmt_counter.fetch_count(), pmt_calib)
 
 
     @kernel(flags={"fast-math"})
@@ -118,7 +141,6 @@ class LaserScan(EnvExperiment):
         self.core.break_realtime()
 
         # initialize dds board
-        #self.dds_board.init()
         self.core.break_realtime()
 
         # initialize qubit AOM and set waveform
@@ -128,13 +150,15 @@ class LaserScan(EnvExperiment):
         self.core.break_realtime()
         self.dds_qubit.cfg_sw(1)
 
+        # turn all core lasers on
+        self.urukul1_cpld.cfg_switches.cfg_switches(0b1110)
+
     @rpc(flags={"async"})
-    def update_dataset(self, trial_num, freq_mhz, pmt_counts, pmt_calib):
+    def update_dataset(self, time_mu, freq_mhz, pmt_counts, pmt_calib):
         """
         Records values via rpc to minimize kernel overhead.
         """
-        self.append_to_dataset('laser_scan', [trial_num, freq_mhz, pmt_counts, pmt_calib])
-        #self.append_to_dataset('tmp', pmt_counts)
+        self.append_to_dataset('laser_scan', [self.core.mu_to_seconds(time_mu), freq_mhz, pmt_counts, pmt_calib])
 
     def analyze(self):
         """
