666040b96fe6c48ec7d5577dc70798f72f4dc4de
Clayton Ho <claytonho@g.ucla.edu>
2022-10-03 17:45:07 -0700

    rabi flopping rdx experiment works

--- experiments/rabi_flopping_rdx.py
+++ experiments/rabi_flopping_rdx.py
@@ -22,14 +22,14 @@ class RabiFloppingRDX(EnvExperiment):
         self.setattr_device("core_dma")
 
         # experiment runs
-        self.setattr_argument("repetitions",                    NumberValue(default=2, ndecimals=0, step=1, min=1, max=10000))
+        self.setattr_argument("repetitions",                    NumberValue(default=100, ndecimals=0, step=1, min=1, max=10000))
 
         # timing
-        self.setattr_argument("time_profileswitch_delay_us",    NumberValue(default=500, ndecimals=5, step=1, min=1, max=10000))
+        self.setattr_argument("time_profileswitch_delay_us",    NumberValue(default=1, ndecimals=5, step=1, min=1, max=10000))
         self.setattr_argument("time_repump_qubit_us",           NumberValue(default=100, ndecimals=5, step=1, min=1, max=10000))
-        self.setattr_argument("time_cooling_us",                NumberValue(default=100, ndecimals=5, step=1, min=1, max=10000))
-        self.setattr_argument("time_cooling_readout_delay_us",  NumberValue(default=100, ndecimals=5, step=1, min=1, max=10000))
-        self.setattr_argument("time_readout_us",                NumberValue(default=100, ndecimals=5, step=1, min=1, max=10000))
+        self.setattr_argument("time_cooling_us",                NumberValue(default=200, ndecimals=5, step=1, min=1, max=10000))
+        self.setattr_argument("time_cooling_readout_delay_us",  NumberValue(default=1000, ndecimals=5, step=1, min=1, max=10000))
+        self.setattr_argument("time_readout_us",                NumberValue(default=500, ndecimals=5, step=1, min=1, max=10000))
 
         # AOM DDS channels
         self.setattr_argument("dds_board_num",                  NumberValue(default=1, ndecimals=0, step=1, min=0, max=1))
@@ -38,27 +38,27 @@ class RabiFloppingRDX(EnvExperiment):
         self.setattr_argument("dds_repump_qubit_channel",       NumberValue(default=3, ndecimals=0, step=1, min=0, max=3))
 
         # AOM DDS channels - qubit
-        self.setattr_argument("dds_board_qubit_num",            NumberValue(default=1, ndecimals=0, step=1, min=0, max=3))
+        self.setattr_argument("dds_board_qubit_num",            NumberValue(default=0, ndecimals=0, step=1, min=0, max=3))
         self.setattr_argument("dds_qubit_channel",              NumberValue(default=0, ndecimals=0, step=1, min=0, max=3))
 
         # AOM DDS parameters
-        self.setattr_argument("freq_pump_cooling_mhz",          NumberValue(default=110, ndecimals=3, step=1, min=10, max=200))
-        self.setattr_argument("freq_pump_readout_mhz",          NumberValue(default=110, ndecimals=3, step=1, min=10, max=200))
+        self.setattr_argument("freq_pump_cooling_mhz",          NumberValue(default=90, ndecimals=3, step=1, min=10, max=200))
+        self.setattr_argument("freq_pump_readout_mhz",          NumberValue(default=92, ndecimals=3, step=1, min=10, max=200))
         self.setattr_argument("freq_repump_cooling_mhz",        NumberValue(default=110, ndecimals=3, step=1, min=10, max=200))
         self.setattr_argument("freq_repump_qubit_mhz",          NumberValue(default=110, ndecimals=3, step=1, min=10, max=200))
-        self.setattr_argument("freq_qubit_mhz",                 NumberValue(default=110, ndecimals=3, step=1, min=10, max=200))
+        self.setattr_argument("freq_qubit_mhz",                 NumberValue(default=105, ndecimals=3, step=1, min=10, max=200))
 
         self.setattr_argument("ampl_pump_pct",                  NumberValue(default=50, ndecimals=3, step=1, min=1, max=100))
         self.setattr_argument("ampl_repump_cooling_pct",        NumberValue(default=50, ndecimals=3, step=1, min=1, max=100))
         self.setattr_argument("ampl_repump_qubit_pct",          NumberValue(default=50, ndecimals=3, step=1, min=1, max=100))
         self.setattr_argument("ampl_qubit_pct",                 NumberValue(default=50, ndecimals=3, step=1, min=1, max=100))
 
-        self.setattr_argument("att_pump_cooling_dB",            NumberValue(default=21.5, ndecimals=1, step=0.5, min=8, max=31.5))
-        self.setattr_argument("att_pump_readout_dB",            NumberValue(default=21.5, ndecimals=1, step=0.5, min=8, max=31.5))
+        self.setattr_argument("att_pump_cooling_dB",            NumberValue(default=23, ndecimals=1, step=0.5, min=8, max=31.5))
+        self.setattr_argument("att_pump_readout_dB",            NumberValue(default=21, ndecimals=1, step=0.5, min=8, max=31.5))
 
         # qubit time scan
         self.setattr_argument("time_rabi_us_list",              Scannable(default=
-                                                                          RangeScan(1, 1000, 21),
+                                                                          RangeScan(5, 1000, 200),
                                                                           global_min=1, global_max=100000, global_step=1,
                                                                           unit="us", scale=1, ndecimals=0
                                                                           ))
@@ -85,7 +85,7 @@ class RabiFloppingRDX(EnvExperiment):
         # rabi flopping timing
         self.time_rabi_mu_list =    [self.core.seconds_to_mu(time_us * us) for time_us in self.time_rabi_us_list]
         self.time_delay_mu_list =   [self.core.seconds_to_mu((self.time_cooling_readout_delay_us - time_us) * us) for time_us in self.time_rabi_us_list]
-        self.num_time_points_list = list(range(len(self.time_rabi_mu)))
+        self.num_time_points_list = list(range(len(self.time_rabi_mu_list)))
 
         # DDS devices
         self.dds_board =            self.get_device("urukul{:d}_cpld".format(self.dds_board_num))
@@ -116,7 +116,10 @@ class RabiFloppingRDX(EnvExperiment):
         self.att_readout_mu = np.int32(0xFF) - np.int32(round(self.att_pump_readout_dB * 8))
 
         # set up datasets
-        self.set_dataset("rabi_flopping", [], broadcast=True)
+        self.set_dataset("rabi_flopping_rdx", [], broadcast=True)
+        self.setattr_dataset("rabi_flopping_rdx")
+        self.set_dataset("rabi_flopping_rdx_processed", np.zeros([len(self.time_rabi_mu_list), 2]))
+        self.setattr_dataset("rabi_flopping_rdx_processed")
 
     @kernel(flags={"fast-math"})
     def run(self):
@@ -239,11 +242,28 @@ class RabiFloppingRDX(EnvExperiment):
         """
         Records values via rpc to minimize kernel overhead.
         """
-        self.append_to_dataset('rabi_flopping', [self.core.mu_to_seconds(time_mu), pmt_counts])
+        self.append_to_dataset('rabi_flopping_rdx', [self.core.mu_to_seconds(time_mu), pmt_counts])
 
     def analyze(self):
         """
         Analyze the results from the experiment.
         """
-        pass
-        # todo: see if we can process data here instead, e.g. converting units
+        # turn dataset into numpy array for ease of use
+        self.rabi_flopping_rdx = np.array(self.rabi_flopping_rdx)
+
+        # get sorted x-values (time, seconds)
+        time_list_s = sorted(set(self.rabi_flopping_rdx[:, 0]))
+
+        # collate results
+        collated_results = {
+            time: []
+            for time in time_list_s
+        }
+        for time_s, pmt_counts in self.rabi_flopping_rdx:
+            collated_results[time_s].append(pmt_counts)
+
+        # process counts for mean and std and put into processed dataset
+        for i, (time_s, count_list) in enumerate(collated_results.items()):
+            self.rabi_flopping_rdx_processed[i] = np.array([time_s, np.mean(count_list)])
+
+        print(self.rabi_flopping_rdx_processed)
