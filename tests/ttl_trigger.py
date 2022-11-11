6ee42ac4127a23181154d79a17daba3fc67206ad
Clayton Ho <claytonho@g.ucla.edu>
2022-11-10 23:20:58 -0800

    forked tmp into a working trigger exp

--- tests/ttl_trigger.py
+++ tests/ttl_trigger.py
@@ -1,9 +1,8 @@
 import numpy as np
 from artiq.experiment import *
-from artiq.coredevice.urukul import urukul_sta_rf_sw, SPI_CONFIG
 
 
-class Testing(EnvExperiment):
+class TTLTrigger(EnvExperiment):
     """
     tmp exp
     Testing
@@ -30,28 +29,42 @@ class Testing(EnvExperiment):
     def run(self):
         self.core.reset()
 
+        # set ttl direction
         self.ttl0.input()
         self.ttl3.input()
         self.core.break_realtime()
 
+
+        # MAIN LOOP
         for i in range(100):
+
+            # wait for event
             time_end_mu = self.ttl0.gate_rising_mu(self.time0)
             time_input_mu = self.ttl0.timestamp_mu(time_end_mu)
 
+            # check if event has fired
             if time_input_mu > 0:
+
+                # set RTIO time and add slack
                 at_mu(time_input_mu)
                 delay_mu(self.timed)
+
+                # get timestamp of RF event
                 time_end_mu2 = self.ttl3.gate_rising_mu(self.time1)
                 time_input_mu2 = self.ttl3.timestamp_mu(time_end_mu2)
-                self.ttl3.count(time_end_mu2)
 
+                # close input gating
+                self.ttl3.count(time_end_mu2)
                 self.ttl0.count(time_end_mu)
                 self.core.break_realtime()
 
+                # add data to dataset
                 self.append_to_dataset("tmp", [time_input_mu, time_input_mu2])
                 self.core.break_realtime()
 
     def analyze(self):
+        # get photon correlation time
         self.tmp = np.array([self.core.mu_to_seconds(val[1] - val[0]) for val in self.tmp])
+        # remove constant offset
+        self.tmp -= np.amin(self.tmp)
         print(self.tmp)
-        print(len(self.tmp))
