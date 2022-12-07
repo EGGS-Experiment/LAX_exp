f1e69d6789e83c5e5e2115863567200a26195a2b
Clayton Ho <claytonho@g.ucla.edu>
2022-12-06 21:05:17 -0800

    testing

--- tmp2.py
+++ tmp2.py
@@ -13,6 +13,7 @@ class Testing(EnvExperiment):
     def build(self):
         self.setattr_device("core")
         self.setattr_device("core_dma")
+        self.setattr_argument("test1", PYONValue([1,2,3]))
 
         # self.set_dataset('dds_tickle_channel', 3, broadcast=True, persist=True)
         # self.set_dataset('ampl_qubit_pct', 50.0, broadcast=True, persist=True)
@@ -22,4 +23,5 @@ class Testing(EnvExperiment):
     def run(self):
         #self.set_dataset('dds.dds_board_tickle_num', 0, broadcast=True, persist=True)
         #self.set_dataset('dds.dds_tickle_channel', 3, broadcast=True, persist=True)
-        self.set_dataset('pmt_gating_edge', 'rising', broadcast=True, persist=True)
+        #self.set_dataset('pmt_gating_edge', 'rising', broadcast=True, persist=True)
+        print(self.test1)
