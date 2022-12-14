6c3b0019a58f8e506478aea16fe974548f97eda6
Clayton Ho <claytonho@g.ucla.edu>
2022-12-14 11:43:24 -0800

    fixed previous bug

--- extensions/device_db_ext.py
+++ extensions/device_db_ext.py
@@ -5,51 +5,51 @@ device_db_ext = {
     # BEAMS
     "probe": {
         "type": "local",
-        "module": "LAX_exp.LAX.devices.beam_397_probe",
+        "module": "LAX_exp.system.devices.beam_397_probe",
         "class": "Beam397Probe"
     },
     "pump": {
         "type": "local",
-        "module": "LAX_exp.LAX.devices.beam_397_pump",
+        "module": "LAX_exp.system.devices.beam_397_pump",
         "class": "Beam397Pump"
     },
     "repump_cooling": {
         "type": "local",
-        "module": "LAX_exp.LAX.devices.beam_866",
+        "module": "LAX_exp.system.devices.beam_866",
         "class": "Beam866"
     },
     "repump_qubit": {
         "type": "local",
-        "module": "LAX_exp.LAX.devices.beam_854",
+        "module": "LAX_exp.system.devices.beam_854",
         "class": "Beam854"
     },
     "qubit": {
         "type": "local",
-        "module": "LAX_exp.LAX.devices.beam_729",
+        "module": "LAX_exp.system.devices.beam_729",
         "class": "Beam729"
     },
     "tickle": {
         "type": "local",
-        "module": "LAX_exp.LAX.devices.beam_tickle",
+        "module": "LAX_exp.system.devices.beam_tickle",
         "class": "BeamTickle"
     },
 
     # PMT
     "pmt": {
         "type": "local",
-        "module": "LAX_exp.LAX.devices.pmt",
+        "module": "LAX_exp.system.devices.pmt",
         "class": "PMTCounter"
     },
 
     # TTL
     "linetrigger": {
         "type": "local",
-        "module": "LAX_exp.LAX.devices.trigger_linetrigger",
+        "module": "LAX_exp.system.devices.trigger_linetrigger",
         "class": "Linetrigger"
     },
     "rf_sync": {
         "type": "local",
-        "module": "LAX_exp.LAX.devices.trigger_rf_modulation",
+        "module": "LAX_exp.system.devices.trigger_rf_modulation",
         "class": "RFSync"
     }
 }
