89af6e79ae97838208250741f8d155b0f1432cb5
Clayton Ho <claytonho@g.ucla.edu>
2022-12-14 13:57:55 -0800

    updated locking read to save values in v instead of mu

--- utilities/locking_read.py
+++ utilities/locking_read.py
@@ -17,45 +17,44 @@ class locking_read(EnvExperiment):
         # devices
         self.setattr_device('core')
 
-        # channels
-        self.setattr_argument("channel_list",           PYONValue([0, 1, 2, 3]))
-        self.setattr_argument("gain_list",              PYONValue([2, 2, 3, 0]))
+        # channels & gains
+        self.setattr_argument("channel_gain_dict",              PYONValue({0: 100, 1: 100, 2:100}), tooltip='gthkim')
 
         # timing
-        self.setattr_argument("sample_rate_hz",         NumberValue(default=5000, ndecimals=3, step=1, min=1, max=5100))
-        self.setattr_argument("time_total_s",           NumberValue(default=10, ndecimals=0, step=1, min=1, max=100000))
+        self.setattr_argument("sample_rate_hz",                 NumberValue(default=5000, ndecimals=3, step=1, min=1, max=5100))
+        self.setattr_argument("time_total_s",                   NumberValue(default=1, ndecimals=0, step=1, min=1, max=100000))
 
 
     def prepare(self):
         # general
-        self.channel_iter =                             list(range(len(self.channel_list)))
+        self.channel_list =                                     list(self.channel_gain_dict.keys())
+        self.channel_iter =                                     list(range(len(self.channel_gain_dict)))
+        self.gain_list_mu =                                     [int(np.log10(gain_mu)) for gain_mu in self.channel_gain_dict.values()]
+        self.adc_mu_to_v_list =                                 np.array([10 / (2**15 * gain_mu) for gain_mu in self.channel_gain_dict.values()])
 
         # ADC
-        self.adc =                                      self.get_device("sampler0")
+        self.adc =                                              self.get_device("sampler0")
 
         # timing
-        self.time_delay_mu =                            self.core.seconds_to_mu(1 / self.sample_rate_hz)
-        self.repetitions =                              np.int32(self.time_total_s * self.sample_rate_hz)
+        self.time_delay_mu =                                    self.core.seconds_to_mu(1 / self.sample_rate_hz)
+        self.repetitions =                                      np.int32(self.time_total_s * self.sample_rate_hz)
 
         # datasets
-        self.set_dataset('locking_readout',             np.zeros([self.repetitions, len(self.channel_list)]))
+        self.set_dataset('locking_readout',                     np.zeros([self.repetitions, len(self.channel_list)]))
         self.setattr_dataset('locking_readout')
 
         # save parameters
-        self.set_dataset('sample_rate_hz',              self.sample_rate_hz)
-        self.set_dataset('time_total_s',                self.time_total_s)
+        self.set_dataset('sample_rate_hz',                      self.sample_rate_hz)
+        self.set_dataset('time_total_s',                        self.time_total_s)
 
 
     @kernel(flags='fast-math')
     def run(self):
         self.core.reset()
 
-        # set up ADC
-        self.adc.init()
-
         # set ADC channel gains
         for i in self.channel_iter:
-            self.adc.set_gain_mu(self.channel_list[i], self.gain_list[i])
+            self.adc.set_gain_mu(self.channel_list[i], self.gain_list_mu[i])
             self.core.break_realtime()
 
         # create holding buffer
@@ -75,19 +74,20 @@ class locking_read(EnvExperiment):
         """
         Records values via rpc to minimize kernel overhead.
         """
-        self.mutate_dataset("locking_readout", i, np.array(volts_mu_arr)[self.channel_list])
+        data = np.array(volts_mu_arr)[self.channel_list] * self.adc_mu_to_v_list
+        self.mutate_dataset("locking_readout", i, data)
 
 
     def analyze(self):
         # print out noise values
+        print(' Results:')
         for i in self.channel_iter:
-            dataset_tmp = adc_mu_to_volt(self.locking_readout[:, i], self.gain_list[i])
-            print('\tch {:d}: {:.3f} +/- {:.3f} mV'.format(self.channel_list[i], np.mean(dataset_tmp) * 1000, np.std(dataset_tmp) * 1000))
-
-        # convert from mu to volts
-        locking_readout_tmp = self.locking_readout.transpose()
-        locking_readout_tmp = np.array([
-            adc_mu_to_volt(locking_readout_tmp[ind], self.gain_list[ind])
-            for ind in self.channel_iter]
-        ).transpose()
-        self.locking_readout = locking_readout_tmp
+            print('\tch {:d}: {:.3f} +/- {:.3f} mV'.format(self.channel_list[i], np.mean(self.locking_readout[:, i]) * 1000, np.std(self.locking_readout[:, i]) * 1000))
+        #
+        # # convert from mu to volts
+        # locking_readout_tmp = self.locking_readout.transpose()
+        # locking_readout_tmp = np.array([
+        #     adc_mu_to_volt(locking_readout_tmp[ind], self.gain_list[ind])
+        #     for ind in self.channel_iter]
+        # ).transpose()
+        # self.locking_readout = locking_readout_tmp
