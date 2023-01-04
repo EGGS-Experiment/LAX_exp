7b8d25954ee968952f4fd0e5ad043ee8efe76931
Clayton Ho <claytonho@g.ucla.edu>
2023-01-03 23:10:25 -0800

    cleaned up coutner read, now works 100

--- utilities/counter_read.py
+++ utilities/counter_read.py
@@ -7,6 +7,7 @@ _DMA_HANDLE = 'PMT_exp'
 class counter_read(EnvExperiment):
     """
     Counter Read
+
     Read TTL counts over time.
     """
     kernel_invariants = {
@@ -22,13 +23,13 @@ class counter_read(EnvExperiment):
         self.setattr_device("core_dma")
 
         # timing
-        self.setattr_argument('time_total_s',                   NumberValue(default=10, ndecimals=6, step=1, min=0, max=100000))
-        self.setattr_argument('time_bin_us',                    NumberValue(default=500, ndecimals=3, step=1, min=0.01, max=1000))
-        self.setattr_argument("sample_rate_hz",                 NumberValue(default=1000, ndecimals=3, step=1, min=1, max=100000))
+        self.setattr_argument('time_total_s',                       NumberValue(default=1000, ndecimals=6, step=1, min=0, max=100000))
+        self.setattr_argument('time_bin_us',                        NumberValue(default=500, ndecimals=3, step=1, min=0.01, max=1000))
+        self.setattr_argument("sample_rate_hz",                     NumberValue(default=1000, ndecimals=3, step=1, min=1, max=100000))
 
         # PMT
-        self.setattr_argument("pmt_input_channel",              NumberValue(default=6, ndecimals=0, step=1, min=0, max=7))
-        self.setattr_argument("pmt_gating_edge",                EnumerationValue(["rising", "falling", "both"], default="rising"))
+        self.setattr_argument("pmt_input_channel",                  NumberValue(default=0, ndecimals=0, step=1, min=0, max=7))
+        self.setattr_argument("pmt_gating_edge",                    EnumerationValue(["rising", "falling", "both"], default="rising"))
 
 
     def prepare(self):
@@ -36,22 +37,19 @@ class counter_read(EnvExperiment):
         Set up the dataset and prepare things such that the kernel functions have minimal overhead.
         """
         # PMT devices
-        self.pmt_counter =                                      self.get_device("ttl{:d}_counter".format(self.pmt_input_channel))
-        self.pmt_gating_edge =                                  getattr(self.pmt_counter, 'gate_{:s}_mu'.format(self.pmt_gating_edge))
+        self.pmt_counter =                                          self.get_device("ttl{:d}_counter".format(self.pmt_input_channel))
+        self.pmt_gating_edge =                                      getattr(self.pmt_counter, 'gate_{:s}_mu'.format(self.pmt_gating_edge))
 
         # iterators
-        self.loop_iter =                                        arange(self.time_total_s * self.sample_rate_hz, dtype=int32)
+        self.loop_iter =                                            arange(self.time_total_s * self.sample_rate_hz, dtype=int32)
 
         # timing
-        self.time_bin_mu =                                      self.core.seconds_to_mu(self.time_bin_us * us)
-        self.time_reset_mu =                                    self.core.seconds_to_mu(1 / self.sample_rate_hz)
+        self.time_bin_mu =                                          self.core.seconds_to_mu(self.time_bin_us * us)
+        self.time_reset_mu =                                        self.core.seconds_to_mu(1 / self.sample_rate_hz - self.time_bin_us * us)
 
         # set up datasets
-        self.set_dataset('pmt_dataset',                         zeros(len(self.loop_iter)))
+        self.set_dataset('pmt_dataset',                             zeros(len(self.loop_iter)))
         self.setattr_dataset('pmt_dataset')
-        #self.set_dataset('thkim', [])
-        #self.setattr_dataset('thkim')
-        self.timeth = self.core.seconds_to_mu(1 * s)
 
     @kernel
     def run(self):
@@ -68,12 +66,9 @@ class counter_read(EnvExperiment):
         handle = self.core_dma.get_handle(_DMA_HANDLE)
         self.core.break_realtime()
 
-        # tmp
-        self.pmt_gating_edge(self.timeth)
-        print(self.pmt_counter.fetch_count())
-
         # run the experiment
         for i in self.loop_iter:
+
             # run pulse sequence from core DMA
             self.core_dma.playback_handle(handle)
 
@@ -96,4 +91,4 @@ class counter_read(EnvExperiment):
         Analyze the results from the experiment.
         """
         # print results
-        print('\tcounts: {:.3f} +/- {:.3f}'.format(mean(self.pmt_dataset), std(self.pmt_dataset)))
+        print('\tCounts: {:.3f} +/- {:.3f}'.format(mean(self.pmt_dataset), std(self.pmt_dataset)))
