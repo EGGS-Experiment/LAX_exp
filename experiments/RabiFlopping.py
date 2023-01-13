176f6c081ef01df60577682808f11165e4a62441
Clayton Ho <claytonho@g.ucla.edu>
2023-01-13 02:47:30 -0800

    things work

--- experiments/RabiFlopping.py
+++ experiments/RabiFlopping.py
@@ -15,14 +15,17 @@ class RabiFlopping2(LAXExperiment, Experiment):
     name = 'Rabi Flopping 2'
 
     def build_experiment(self):
+        # core arguments
+        self.setattr_argument("repetitions",                        NumberValue(default=100, ndecimals=0, step=1, min=1, max=10000))
+
         # rabi flopping parameters
         self.setattr_argument("time_rabi_us_list",                  Scannable(
-                                                                        default=RangeScan(0, 400, 201, randomize=True),
+                                                                        default=RangeScan(0, 400, 401, randomize=True),
                                                                         global_min=1, global_max=100000, global_step=1,
                                                                         unit="us", scale=1, ndecimals=5
                                                                     ))
 
-        self.setattr_argument("freq_rabiflop_mhz",                  NumberValue(default=104.3895, ndecimals=5, step=1, min=1, max=10000))
+        self.setattr_argument("freq_rabiflop_mhz",                  NumberValue(default=104.335, ndecimals=5, step=1, min=1, max=10000))
 
     def prepare_experiment(self):
         # get devices
