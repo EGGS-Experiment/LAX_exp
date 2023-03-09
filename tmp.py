f35fe457e57e8c906d5a83fd76b7817074300139
Clayton Ho <claytonho@g.ucla.edu>
2023-03-09 15:07:24 -0800

    minor updates

--- tmp.py
+++ tmp.py
@@ -2,6 +2,7 @@ import labrad
 import numpy as np
 from os import environ
 from artiq.experiment import *
+from datetime import datetime
 from artiq.coredevice.urukul import urukul_sta_rf_sw, SPI_CONFIG
 from artiq.coredevice.rtio import (rtio_output, rtio_input_timestamp,
                                    rtio_input_data)
@@ -32,27 +33,39 @@ class testarg12(EnvExperiment):
         # self.set_dataset('ampl_qubit_pct', 50.0, broadcast=True, persist=True)
         # self.set_dataset('ampl_repump_cooling_pct', 10.0, broadcast=True, persist=True)
 
+        calib_timestamp = datetime.timestamp(datetime.now())
+        th0 = np.arange(85,135,2)
+        th1 = np.array([0.15625, 0.140625, 0.125, 0.1171875, 0.109375, 0.109375,
+                        0.109375, 0.1171875, 0.1171875, 0.109375, 0.109375, 0.109375,
+                        0.1171875, 0.125, 0.125, 0.125, 0.1328125, 0.140625, 0.140625,
+                        0.15625, 0.171875, 0.203125, 0.25, 0.28125, 0.34375]) * 100
+
+        self.set_dataset('calibration.temperature.asf_calibration_curve_mhz_pct', np.array([th0, th1]).transpose(), broadcast=True, persist=True)
+        self.set_dataset('calibration.temperature.calibration_timestamp', calib_timestamp, broadcast=True, persist=True)
+
     def prepare(self):
-        self.setattr_device('urukul0_cpld')
-        self.setattr_device('urukul0_ch0')
-        self.delayth = np.int64(100)
+        pass
+        # self.setattr_device('urukul0_cpld')
+        # self.setattr_device('urukul0_ch0')
+        # self.delayth = np.int64(100)
 
 
     # @kernel(flags={"fast-math"})
-    @kernel
+   # @kernel
     def run(self):
-        self.core.reset()
-        #self.core.break_realtime()
-
-        #at_mu(now_mu())
-        self.urukul0_ch0.set_mu(472446402, asf=3276)
-        #delay_mu(10)
-        self.urukul0_ch0.set_mu(515396075, asf=3276)
-        #delay_mu(10)
-        self.urukul0_ch0.set_mu(558345748, asf=3276)
-        #delay_mu(10)
-
-        self.core.break_realtime()
+        pass
+        # self.core.reset()
+        # #self.core.break_realtime()
+        #
+        # #at_mu(now_mu())
+        # self.urukul0_ch0.set_mu(472446402, asf=3276)
+        # #delay_mu(10)
+        # self.urukul0_ch0.set_mu(515396075, asf=3276)
+        # #delay_mu(10)
+        # self.urukul0_ch0.set_mu(558345748, asf=3276)
+        # #delay_mu(10)
+        #
+        # self.core.break_realtime()
 
     @kernel
     def recordDMA(self):
