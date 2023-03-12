719eea2cdbd6df489c8d327c8bcbd52713a11a79
Clayton Ho <claytonho@g.ucla.edu>
2023-03-12 03:09:06 -0700

    added line triggering to laser scan

--- experiments_legacy/laser_scan.py
+++ experiments_legacy/laser_scan.py
@@ -53,21 +53,21 @@ class LaserScan(EnvExperiment):
         self.setattr_device("core_dma")
 
         # experiment runs
-        self.setattr_argument("repetitions",                            NumberValue(default=10, ndecimals=0, step=1, min=1, max=10000))
+        self.setattr_argument("repetitions",                            NumberValue(default=40, ndecimals=0, step=1, min=1, max=10000))
 
         # additional cooling
-        self.setattr_argument("repetitions_per_cooling",                NumberValue(default=1, ndecimals=0, step=1, min=1, max=10000))
+        self.setattr_argument("repetitions_per_cooling",                NumberValue(default=200, ndecimals=0, step=1, min=1, max=10000))
         self.setattr_argument("additional_cooling_time_s",              NumberValue(default=1, ndecimals=5, step=0.1, min=0, max=10000))
 
         # adc recording
         self.setattr_argument("adc_channel_gain_dict",                  PYONValue({0: 1000, 1: 10}))
 
         # timing
-        self.setattr_argument("time_729_us",                            NumberValue(default=400, ndecimals=5, step=1, min=1, max=10000000))
+        self.setattr_argument("time_729_us",                            NumberValue(default=5000, ndecimals=5, step=1, min=1, max=10000000))
 
         # frequency scan
         self.setattr_argument("freq_qubit_scan_mhz",                    Scannable(
-                                                                            default=CenterScan(104.39, 0.4, 0.001, randomize=True),
+                                                                            default=CenterScan(103.77, 0.02, 0.00005, randomize=True),
                                                                             global_min=60, global_max=200, global_step=1,
                                                                             unit="MHz", scale=1, ndecimals=5
                                                                         ))
@@ -85,6 +85,11 @@ class LaserScan(EnvExperiment):
         # PMT devices
         self.pmt_counter =                                      self.get_device("ttl{:d}_counter".format(self.pmt_input_channel))
         self.pmt_gating_edge =                                  getattr(self.pmt_counter, 'gate_{:s}_mu'.format(self.pmt_gating_edge))
+
+        # linetrigger
+        self.linetrigger =                                      self.get_device("ttl5")
+        self.time_linetrigger_timeout_mu =                      self.core.seconds_to_mu(30 * ms)
+        self.time_linetrigger_holdoff_mu =                      self.core.seconds_to_mu(5 * us)
 
         # ADC
         self.adc =                                              self.get_device("sampler0")
@@ -174,17 +179,29 @@ class LaserScan(EnvExperiment):
                 self.dds_qubit.set_mu(freq_ftw, asf=self.ampl_qubit_asf)
                 self.core.break_realtime()
 
-                # run sequence
-                self.core_dma.playback_handle(handle)
+                # wait for linetrigger to read rising edge
+                time_linetrigger_wait_mu = self.linetrigger.gate_rising_mu(self.time_linetrigger_timeout_mu)
+                if self.linetrigger.timestamp_mu(time_linetrigger_wait_mu) > 0:
 
-                # get ADC values
-                self.adc.sample_mu(sampler_buffer)
-                self.core.break_realtime()
+                    # set time to now, with some additional holdoff
+                    at_mu(time_linetrigger_wait_mu + self.time_linetrigger_holdoff_mu)
 
-                # update dataset
-                with parallel:
-                    self.update_dataset(freq_ftw, self.pmt_counter.fetch_count(), now_mu(), sampler_buffer)
+                    with parallel:
+
+                        # stop linetrigger input
+                        self.linetrigger._set_sensitivity(0)
+
+                        # run sequence
+                        self.core_dma.playback_handle(handle)
+
+                    # get ADC values
+                    self.adc.sample_mu(sampler_buffer)
                     self.core.break_realtime()
+
+                    # update dataset
+                    with parallel:
+                        self.update_dataset(freq_ftw, self.pmt_counter.fetch_count(), now_mu(), sampler_buffer)
+                        self.core.reset()
 
             # add post repetition cooling
             if (trial_num > 0) and (trial_num % self.repetitions_per_cooling == 0):
