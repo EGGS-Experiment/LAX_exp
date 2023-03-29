3ee9fb44b5396ebdb918c73db38841e4a5d6c970
Clayton Ho <claytonho@g.ucla.edu>
2023-03-29 01:45:01 -0700

    fixed bug in exp batch scheduler

--- tmp2.py
+++ tmp2.py
@@ -19,33 +19,31 @@ class ScheduleTest(EnvExperiment):
         self.scan_range_v = list(self.scan_range_v)
         self.expid_list = list()
 
-        expid = {
-            "file": "experiments_legacy\\parametric_sweep.py",
-            "class_name": "ParametricSweep",
-            "log_level": 30,
-            "repo_rev": "1e672b2317d87dd0e70d49a8f1f1639d3fd217b8",
-            "arguments": {
-                "num_counts": 10000,
-                "ampl_mod_vpp": 0.05,
-                "freq_mod_mhz_list": {
-                    "center": 1.206,
-                    "span": 0.02,
-                    "step": 0.0002,
-                    "randomize": True,
-                    "seed": None,
-                    "ty": "CenterScan"
-                },
-                "dc_micromotion_channel": "V Shim",
-                "dc_micromotion_voltage_v": 30.0
-            }
-        }
-
         for scan_val_v in self.scan_range_v:
-            expid_tmp = expid.copy()
-            expid_tmp["arguments"]["dc_micromotion_voltage_v"] = scan_val_v
-            self.expid_list.append(expid_tmp)
+            expid = {
+                "file": "experiments_legacy\\parametric_sweep.py",
+                "class_name": "ParametricSweep",
+                "arguments": {
+                    "num_counts": 10000,
+                    "ampl_mod_vpp": 0.05,
+                    "freq_mod_mhz_list": {
+                        "center": 1.206,
+                        "span": 0.04,
+                        "step": 0.0002,
+                        "randomize": True,
+                        "seed": None,
+                        "ty": "CenterScan"
+                    },
+                    "dc_micromotion_channel": "V Shim",
+                    "dc_micromotion_voltage_v": scan_val_v
+                },
+                "log_level": 30,
+                "repo_rev": "1e672b2317d87dd0e70d49a8f1f1639d3fd217b8"
+            }
+            self.expid_list.append(expid)
 
 
     def run(self):
         for expid_dj in self.expid_list:
+            # print(expid_dj["arguments"]["dc_micromotion_voltage_v"])
             self._scheduler.submit(pipeline_name="main", expid=expid_dj)
