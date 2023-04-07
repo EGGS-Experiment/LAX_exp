52c5386d1158aa99049a1345cedde52335545b3c
Clayton Ho <claytonho@g.ucla.edu>
2023-04-07 15:50:49 -0700

    first draft of rabi flopping dynamic, untested

---
+++ experiments/RabiFloppingDynamic.py
@@ -0,0 +1,142 @@
+import numpy as np
+from artiq.experiment import *
+
+from LAX_exp.extensions import *
+from LAX_exp.base import LAXExperiment
+from LAX_exp.system.subsequences import InitializeQubit, RabiFlop, Readout, RescueIon
+
+_THRESHOLD1 = 56
+_THRESHOLD2 = 141
+
+
+class RabiFloppingDynamic(LAXExperiment, Experiment):
+    """
+    Experiment: Rabi Flopping
+
+    Measures ion fluorescence vs 729nm pulse time and frequency.
+    """
+    name = 'Rabi Flopping Dynamic'
+
+
+    def build_experiment(self):
+        # core arguments
+        self.setattr_argument("repetitions",                        NumberValue(default=20, ndecimals=0, step=1, min=1, max=10000))
+
+        # rabi flopping arguments
+        self.setattr_argument("time_rabi_us_list",                  Scannable(
+                                                                        default=RangeScan(0, 100, 101, randomize=True),
+                                                                        global_min=1, global_max=100000, global_step=1,
+                                                                        unit="us", scale=1, ndecimals=5
+                                                                    ), group=self.name)
+        self.setattr_argument("freq_rabiflop_mhz",                  NumberValue(default=103.9335, ndecimals=5, step=1, min=1, max=10000), group=self.name)
+        self.setattr_argument("att_qubit_db",                       NumberValue(default=8, ndecimals=1, step=0.5, min=8, max=31.5), group=self.name)
+
+
+        # get devices
+        self.setattr_device('qubit')
+
+        # prepare sequences
+        self.initialize_subsequence =                               InitializeQubit(self)
+        self.readout_subsequence =                                  Readout(self)
+        self.rescue_subsequence =                                   RescueIon(self)
+
+    def prepare_experiment(self):
+        # convert rabi flopping arguments
+        max_time_us =                                               np.max(list(self.time_rabi_us_list))
+        self.time_rabiflop_mu_list =                                np.array([
+                                                                        [seconds_to_mu((max_time_us - time_us) * us), seconds_to_mu(time_us * us)]
+                                                                        for time_us in self.time_rabi_us_list
+                                                                    ])
+        self.freq_rabiflop_ftw =                                    hz_to_ftw(self.freq_rabiflop_mhz * MHz)
+
+        # convert attenuation to machine units
+        self.att_qubit_mu =                                         att_to_mu(self.att_qubit_db * dB)
+
+        # tmp remove
+        self.set_dataset('datatmp', np.zeros((len(self.time_rabiflop_mu_list), 2)), broadcast=True,persist=True,archive=True)
+        self.setattr_dataset('datatmp')
+        # tmp remove
+
+    @property
+    def results_shape(self):
+        return (self.repetitions * len(self.time_rabiflop_mu_list),
+                2)
+
+
+    # MAIN SEQUENCE
+    @kernel(flags={"fast-math"})
+    def initialize_experiment(self):
+        self.core.break_realtime()
+
+        # record subsequences onto DMA
+        self.initialize_subsequence.record_dma()
+        self.readout_subsequence.record_dma()
+
+        # set qubit beam parameters
+        self.qubit.set_att_mu(self.att_qubit_mu)
+        self.qubit.set_mu(self.freq_rabiflop_ftw, asf=self.qubit.ampl_qubit_asf)
+        self.core.break_realtime()
+
+    @kernel(flags={"fast-math"})
+    def run_main(self):
+        self.core.reset()
+
+        # tmp remove
+        data_tmp = np.zeros((len(self.time_rabiflop_mu_list, 2)))
+        # tmp remove
+
+        for trial_num in range(self.repetitions):
+
+            # tmp remove
+            _iter_tmp = 0
+            # tmp remove
+
+            # sweep time
+            for time_rabi_pair_mu in self.time_rabiflop_mu_list:
+                self.core.break_realtime()
+
+                # initialize ion in S-1/2 state
+                self.initialize_subsequence.run_dma()
+
+                # wait given time
+                delay_mu(time_rabi_pair_mu[0])
+
+                # rabi flopping w/qubit laser
+                self.qubit.on()
+                delay_mu(time_rabi_pair_mu[1])
+                self.qubit.off()
+
+                # do readout
+                self.readout_subsequence.run_dma()
+
+                # update dataset
+                with parallel:
+                    # tmp remove
+                    with sequential:
+                        data_tmp[_iter_tmp] = time_rabi_pair_mu
+                        _iter_tmp += 1
+                    # tmp remove
+
+                    self.update_results(time_rabi_pair_mu[1], self.readout_subsequence.fetch_count())
+                    self.core.break_realtime()
+
+            # tmp remove
+            with parallel:
+                self.update_results_dynamic(data_tmp, trial_num)
+                self.core.break_realtime()
+            # tmp remove
+
+    # tmp remove
+    @rpc(flags={"async"})
+    def update_results_dynamic(self, data_tmp, n):
+        """
+        todo: document
+        """
+        # get counts
+        counts = np.sort(data_tmp, axis=0)[:, 1]
+        probability = (counts >= _THRESHOLD1) * 0.5 + (counts >= _THRESHOLD2) * 0.5
+
+
+        self.datatmp = (self.datatmp * (n + 1) + probability) / (n + 2)
+    # tmp remove
+
