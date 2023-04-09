0fb0c984d5b23472d5720c33609cceb8ca336600
Clayton Ho <claytonho@g.ucla.edu>
2023-04-09 02:41:45 -0700

    temperature measurement calibration finished

--- calibrations/temperature_measurement_calibration.py
+++ calibrations/temperature_measurement_calibration.py
@@ -23,7 +23,7 @@ class TemperatureMeasurementCalibration(EnvExperiment):
 
         # DDS setup
         self.setattr_argument("dds_freq_mhz_list",                          Scannable(
-                                                                                default=RangeScan(95, 125, 10, randomize=True),
+                                                                                default=RangeScan(95, 125, 20, randomize=True),
                                                                                 global_min=70, global_max=140, global_step=1,
                                                                                 unit="MHz", scale=1, ndecimals=5
                                                                             ))
@@ -32,14 +32,13 @@ class TemperatureMeasurementCalibration(EnvExperiment):
         self.setattr_argument("dds_ampl_max_pct",                           NumberValue(default=50, ndecimals=2, step=1, min=5, max=50))
         self.setattr_argument("dds_attenuation_db",                         NumberValue(default=14, ndecimals=1, step=0.5, min=14, max=31.5))
 
-
         # sampler setup
         self.setattr_argument("adc_channel_num",                            NumberValue(default=6, ndecimals=0, step=1, min=0, max=3))
         self.setattr_argument("adc_gain_num",                               EnumerationValue(['1', '10', '100', '1000'], default='100'))
         self.setattr_argument("adc_sample_num",                             NumberValue(default=100, ndecimals=0, step=1, min=1, max=5000))
 
         # search parameters
-        self.setattr_argument("target_voltage_mv",                          NumberValue(default=46, ndecimals=3, step=1, min=-10000, max=10000))
+        self.setattr_argument("target_voltage_mv",                          NumberValue(default=40, ndecimals=3, step=1, min=-10000, max=10000))
         self.setattr_argument("target_tolerance_mv",                        NumberValue(default=1, ndecimals=3, step=1, min=0, max=1000))
 
 
@@ -50,7 +49,7 @@ class TemperatureMeasurementCalibration(EnvExperiment):
         # todo: checks for ampl range, target val
 
         # tmp remove
-        self.time_start = datetime.timestamp(datetime.now())
+        self.time_start =                                                   datetime.timestamp(datetime.now())
         # tmp remove
 
         # get devices
@@ -71,6 +70,7 @@ class TemperatureMeasurementCalibration(EnvExperiment):
         self.adc_v_to_mu =                                                  (2**15 * self.adc_gain_num) / 10
         self.adc_mu_to_v =                                                  10 / (2**15 * self.adc_gain_num)
         self.adc_gain_mu =                                                  np.int32(np.log10(self.adc_gain_num))
+        self.adc_poll_delay_mu =                                            self.core.seconds_to_mu(200 * us)
 
         # convert target voltage values into ADC machine units
         self.target_voltage_mu =                                            np.int32((self.target_voltage_mv / 1000) * self.adc_v_to_mu)
@@ -97,8 +97,16 @@ class TemperatureMeasurementCalibration(EnvExperiment):
         # scan DDS frequencies
         for freq_ftw in self.dds_freq_ftw_list:
 
+            # tmp remove
+            self.core.break_realtime()
+            # print('\t\t\tNEW RUN: ', freq_ftw * 1e3 / 0xFFFFFFFF)
+            self.core.break_realtime()
+            # delay_mu(250000)
+            # tmp remove
+
             # do a recursion search
             ampl_calib_asf = self._recursion_search(freq_ftw, self.dds_ampl_min_asf, self.dds_ampl_max_asf)
+            self.core.break_realtime()
 
             # set frequency
             with parallel:
@@ -113,12 +121,6 @@ class TemperatureMeasurementCalibration(EnvExperiment):
         """
         # get mid-range value
         ampl_tmp_asf = np.int32((ampl_min_asf + ampl_max_asf) / 2)
-
-        # # tmp remove
-        # self.core.break_realtime()
-        # print('\trecursion called: ', ampl_tmp_asf / 0x3FFF * 100, 2)
-        # self.core.break_realtime()
-        # # tmp remove
 
         # set dds amplitude
         self.dds.set_mu(freq_ftw, asf=ampl_tmp_asf)
@@ -156,11 +158,10 @@ class TemperatureMeasurementCalibration(EnvExperiment):
 
             # calculate simple moving average instead of storing values for efficiency
             sampler_running_avg_mu = np.int32((sampler_running_avg_mu * i + adc_input_tmp_mu) / (i + 1))
-            self.core.break_realtime()
+            delay_mu(self.adc_poll_delay_mu)
 
         # return average
         return sampler_running_avg_mu
-
 
     @kernel(flags={"fast-math"})
     def prepareDevices(self):
@@ -193,7 +194,7 @@ class TemperatureMeasurementCalibration(EnvExperiment):
         """
         # set calibration timestamp
         calib_timestamp = datetime.timestamp(datetime.now())
-        # self.set_dataset('calibration.temperature.calibration_timestamp', calib_timestamp, broadcast=True, persist=True)
+        self.set_dataset('calibration.temperature.calibration_timestamp', calib_timestamp, broadcast=True, persist=True)
 
         # sort results
         _indices_sorted = np.argsort(self.results, axis=0)[:, 0]
@@ -203,16 +204,15 @@ class TemperatureMeasurementCalibration(EnvExperiment):
         calib_freq_ftw, calib_ampl_asf = results_tmp
         calib_freq_mhz = np.array(self.dds.ftw_to_frequency(calib_freq_ftw)) / MHz
         calib_ampl_frac = np.array(self.dds.asf_to_amplitude(calib_ampl_asf)) * 100
+        calib_final = np.array([calib_freq_mhz, calib_ampl_frac]).transpose()
 
         # tmp remove
-        initialize_time = '\tinitialize time: {:.2f}'.format(datetime.timestamp(datetime.now()) - self.time_start)
+        time_stop = datetime.timestamp(datetime.now())
         print()
-        print('\t\t\tFINAL RES: ', initialize_time)
+        print('\t\t\tDONE')
+        print('\t\t\t\tTOTAL RUN TIME: {:.2f}'.format(time_stop-self.time_start))
         print()
-        print(np.array([calib_freq_mhz, calib_ampl_frac]).transpose())
-        print()
-        print(calib_ampl_frac)
         # tmp remove
 
         # add calibration values to dataset manager
-        # self.set_dataset('calibration.temperature.asf_calibration_curve_mhz_pct', np.array([calib_freq_mhz, calib_ampl_frac]).transpose(), broadcast=True, persist=True)
+        self.set_dataset('calibration.temperature.asf_calibration_curve_mhz_pct', np.array([calib_freq_mhz, calib_ampl_frac]).transpose(), broadcast=True, persist=True)
