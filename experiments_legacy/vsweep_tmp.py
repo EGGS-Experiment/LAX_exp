7ada1bb0897d027ab46a53b2a7ee1e8657f33339
Clayton Ho <claytonho@g.ucla.edu>
2023-05-17 17:37:34 -0700

    updated complex fit minimize function in vsweep tmp to use leastsquares

--- experiments_legacy/vsweep_tmp.py
+++ experiments_legacy/vsweep_tmp.py
@@ -2,6 +2,7 @@ import labrad
 import numpy as np
 from time import sleep
 from scipy import stats
+from scipy import optimize
 
 from os import environ
 from artiq.experiment import *
@@ -239,6 +240,8 @@ class vsweeptmp(EnvExperiment):
         voltage_set_v = self.dc.voltage_fast(channel, voltage_v)
         print('\tvoltage set: {}'.format(voltage_set_v))
 
+
+    # ANALYSIS
     @rpc
     def _complexFitMinimize(self, dataset):
         """
@@ -252,29 +255,23 @@ class vsweeptmp(EnvExperiment):
         Returns:
             (float) : the value of the independent variable that minimizes the complex amplitude.
         """
-        # split dataset into IV/DV, and real/imaginary
-        dataset_x = dataset[:, 0]
-        dataset_y = np.array([np.real(dataset[:, 1]), np.imag(dataset[:, 1])]).transpose()
 
-        # fit the DV in the complex plane and get the unit vector of the line
-        fit_complex = stats.linregress(dataset_y[:, 0], dataset_y[:, 1])
-        m_c, b_c = fit_complex.slope, fit_complex.intercept
+        # create norm function for least squares optimization
+        def func_norm(b_params, x, y):
+            return b_params[0] + b_params[1] * x[1] - y
 
-        # get the projection of the DV onto the fitted line
-        vec_complex = np.array([1, m_c]) / np.linalg.norm(np.array([1, m_c]))
-        dataset_proj = np.dot(dataset_y, vec_complex)
+        # guess starting b_params
+        b_guess = [0.1, 0.1]
 
-        # fit the x dataset to the parameterized line
-        fit_parameterized = stats.linregress(dataset_x, dataset_proj)
-        m_p, b_p = fit_parameterized.slope, fit_parameterized.intercept
+        # do a complex least squares fit
+        res = optimize.least_squares(func_norm, b_guess, args=(dataset[:, 0], dataset[:, 1]))
+        res_intercept, res_slope = res.x
 
-        # extract x_min
-        x_min = - (m_c * b_c)/(1 + np.power(m_c, 2))
+        # extract optimal voltage to minimize displacement
+        voltage_optimal = - (np.re(res_intercept) * np.re(res_slope) + np.imag(res_intercept) * np.imag(res_slope)) / (
+            np.power(np.abs(res_slope), 2))
 
-        # convert x_min to V_min
-        V_min = (x_min - b_p) / m_p
-
-        return V_min
+        return voltage_optimal
 
     def analyze(self):
         """
