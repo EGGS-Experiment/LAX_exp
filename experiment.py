e1e4ff7d51b41fb211f1226f5d8bf961f4566872
Clayton Ho <claytonho@g.ucla.edu>
2023-08-23 01:45:35 -0700

    finished first implementation of LAX_exp.experiment a la artiq.experiment; yet untested

--- experiment.py
+++ experiment.py
@@ -1,4 +1,22 @@
-# todo: extensions
-# todo: analysis
-# todo: base
-# todo: import artiq
+"""
+LAX.experiment
+
+Contains all core modules needed to use/run/develop LAX_exp.
+"""
+
+__all__ = []
+
+# import base (core LAX device objects)
+from LAX_exp import base
+from LAX_exp.base import *
+__all__.extend(base.__all__)
+
+# import extensions (useful LAX language extensions)
+from LAX_exp import extensions
+from LAX_exp.extensions import *
+__all__.extend(extensions.__all__)
+
+# import analysis modules
+from LAX_exp import analysis
+from LAX_exp.analysis import *
+__all__.extend(analysis.__all__)
