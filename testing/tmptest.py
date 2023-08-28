aa4b0e4ba6b484b609d4451ee129a02149c26304
Clayton Ho <claytonho@g.ucla.edu>
2023-08-27 21:54:54 -0700

    saving changes

--- testing/tmptest.py
+++ testing/tmptest.py
@@ -12,17 +12,18 @@ from artiq.coredevice.rtio import (rtio_output, rtio_input_timestamp,
 class testidkurukul(EnvExperiment):
     """
     testidkurukul
-    Testing
+    Testing urukul stuff
     """
 
     def build(self):
-        self.frequency_mhz =                350.
+        self.frequency_mhz =                82.
         self.amplitude_pct =                50.
 
         self.phase_turns0 =                 0.
-        self.phase_turns1 =                 0.
+        # self.phase_turns1 =                 -0.325
+        self.phase_turns1 =                 0.5
 
-        self.time_delay_ns =                20
+        self.time_delay_ns =                8
 
     def prepare(self):
         # core devices
@@ -70,11 +71,21 @@ class testidkurukul(EnvExperiment):
 
 
         # calculate phase values to compensate for ch0 & ch1 delays
-        self.time_ch1_delay_ns =            1.66
         self.phase_ch1_inherent_turns =     0.
-        self.phase_ch1_delay_turns =        (self.frequency_mhz * MHz) * (self.time_ch1_delay_ns * ns)
+
+        self.time_ch1_latency_ns =          1.66
+        self.phase_ch1_latency_turns =      (self.frequency_mhz * MHz) * (self.time_ch1_latency_ns * ns)
+
+        print(bin(np.int32(round(self.time_delay_ns))))
+        time_delay_tmp_ns = np.int32(round(self.time_delay_ns)) & ~0x7
+        print(bin(time_delay_tmp_ns))
+        # self.phase_ch1_delay_turns =        (self.frequency_mhz * MHz) * (self.time_delay_ns * ns)
+        self.phase_ch1_delay_turns =        (self.frequency_mhz * MHz) * (time_delay_tmp_ns * ns)
         # combine compensation values into ch1 phase: ch0 vs ch1 inherent phase shift/relation, ch0 vs ch1 inherent time delay, and ch1 offset
-        self.phase_ch1_final_pow =          self.urukul0_ch0.turns_to_pow(self.phase_ch1_inherent_turns + self.phase_ch1_delay_turns + self.phase_turns1)
+        self.phase_ch1_final_pow =          self.urukul0_ch0.turns_to_pow(self.phase_ch1_inherent_turns +
+                                                                          self.phase_ch1_latency_turns +
+                                                                          self.phase_ch1_delay_turns +
+                                                                          self.phase_turns1)
 
 
         # calculate timings
@@ -105,26 +116,28 @@ class testidkurukul(EnvExperiment):
             self.urukul1_ch3.set_att(14 * dB)
 
         # prepare - DDS profiles
-        at_mu(now_mu() + 50000)
+        at_mu(now_mu() + 10000)
         with parallel:
             self.res00 = self.urukul0_ch3.set_mu(self.freq_ftw0, asf=0x01, pow_=0x0, profile=0)
-            self.res10 = self.urukul1_ch3.set_mu(self.freq_ftw1, asf=0x01, pow_=0x0, profile=0)
+            self.res10 = self.urukul1_ch3.set_mu(self.freq_ftw1, asf=0x01, pow_=self.phase_ch1_final_pow, profile=0)
         with parallel:
             self.res01 = self.urukul0_ch3.set_mu(self.freq_ftw0, asf=self.ampl_asf0, pow_=self.phase_ch0_offset_pow, profile=1)
             self.res11 = self.urukul1_ch3.set_mu(self.freq_ftw1, asf=self.ampl_asf1, pow_=self.phase_ch1_final_pow, profile=1)
 
         # set up registers
-        at_mu(now_mu() + 50000)
-        self.set_cfr1_rdx(phase_autoclear=1, select_sine_output=1)
-        # with parallel:
-        #     self.urukul0_ch3.set_cfr1(phase_autoclear=1)
-        #     self.urukul1_ch3.set_cfr1(phase_autoclear=1)
+        at_mu(now_mu() + 10000)
+        # self.set_cfr1_rdx(phase_autoclear=1, select_sine_output=1)
+        with parallel:
+            # self.urukul0_ch3.set_cfr1(phase_autoclear=1)
+            # self.urukul1_ch3.set_cfr1(phase_autoclear=1)
+            self.urukul0_ch3.write32(_AD9910_REG_CFR1, (1 << 16) | (1 << 13))
+            self.urukul1_ch3.write32(_AD9910_REG_CFR1, (1 << 16) | (1 << 13))
         with parallel:
             self.urukul0_ch3.set_cfr2(matched_latency_enable=1)
             self.urukul1_ch3.set_cfr2(matched_latency_enable=1)
 
         # set up for running
-        at_mu(now_mu() + 50000)
+        at_mu(now_mu() + 10000)
         with parallel:
             self.urukul0_cpld.set_profile(0)
             self.urukul1_cpld.set_profile(0)
@@ -147,15 +160,6 @@ class testidkurukul(EnvExperiment):
         time_start_mu = now_mu() & ~7
 
         # RUN
-        # at_mu(time_start_mu)
-        # with parallel:
-        #     with sequential:
-        #         self.urukul0_ch3.set_cfr1(phase_autoclear=1)
-        #         self.urukul0_cpld.io_update.pulse_mu(8)
-        #
-        #     with sequential:
-        #         self.urukul1_ch3.set_cfr1(phase_autoclear=1)
-        #         self.urukul1_cpld.io_update.pulse_mu(8)
 
         # start first waveform
         at_mu(time_start_mu)
@@ -168,8 +172,8 @@ class testidkurukul(EnvExperiment):
                 self.ttl8.on()
 
         # start cancellation waveform
-        # at_mu(time_start_mu + self.time_delay_mu)
-        at_mu((time_start_mu + self.time_delay_mu) & ~0x3)
+        at_mu(time_start_mu + self.time_delay_mu)
+        # at_mu((time_start_mu + self.time_delay_mu) & ~0x3)
         with parallel:
             self.urukul1_cpld.set_profile(1)
 
@@ -203,10 +207,12 @@ class testidkurukul(EnvExperiment):
         # with parallel:
         #     self.urukul0_cpld.set_profile(0)
         #     self.urukul1_cpld.set_profile(0)
-            # self.urukul0_cpld.cfg_switches(0b0000)
-            # self.urukul1_cpld.cfg_switches(0b0000)
         delay_mu(100)
         with parallel:
+            self.urukul0_cpld.set_profile(0)
+            self.urukul1_cpld.set_profile(0)
+            # self.urukul0_cpld.cfg_switches(0b0000)
+            # self.urukul1_cpld.cfg_switches(0b0000)
             self.ttl8.off()
             self.ttl9.off()
         self.core.reset()
@@ -283,4 +289,6 @@ class testidkurukul(EnvExperiment):
         print('\t\turukul1 profile 1 phase: {:.4f}'.format(self.urukul0_ch0.pow_to_turns(self.res11)))
         print('\n')
 
-        print('idk: {}'.format(self.urukul0_ch3.sync_data.io_update_delay))
+        print('\n\tio_update_delay:\t{}'.format(self.urukul0_ch3.sync_data.io_update_delay))
+        print('\n\tsys_clk:\t{}'.format(self.urukul0_ch3.sysclk))
+        print('\n\tsync_clk:\t{}'.format(self.urukul0_ch3.sysclk_per_mu * 4))
