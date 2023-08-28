d8f6e8efaadfce6352242a9ad37ed7b500ead283
Clayton Ho <claytonho@g.ucla.edu>
2023-08-27 18:33:38 -0700

    developed bold and daring new techniques for urukul in testing/tmptest.py

--- testing/tmptest.py
+++ testing/tmptest.py
@@ -5,7 +5,7 @@ from artiq.experiment import *
 from datetime import datetime
 
 from artiq.coredevice.urukul import urukul_sta_rf_sw, SPI_CONFIG
-from artiq.coredevice.ad9910 import PHASE_MODE_ABSOLUTE
+from artiq.coredevice.ad9910 import PHASE_MODE_ABSOLUTE, _AD9910_REG_CFR1
 from artiq.coredevice.rtio import (rtio_output, rtio_input_timestamp,
                                    rtio_input_data)
 
@@ -16,13 +16,13 @@ class testidkurukul(EnvExperiment):
     """
 
     def build(self):
-        self.frequency_mhz = 82.
-        self.amplitude_pct = 50.
+        self.frequency_mhz =                350.
+        self.amplitude_pct =                50.
 
-        self.phase_turns0 = 0.
-        self.phase_turns1 = 0.5
+        self.phase_turns0 =                 0.
+        self.phase_turns1 =                 0.
 
-        self.time_delay_ns = 20
+        self.time_delay_ns =                20
 
     def prepare(self):
         # core devices
@@ -54,62 +54,83 @@ class testidkurukul(EnvExperiment):
         self.setattr_device("urukul2_ch2")
         self.setattr_device("urukul2_ch3")
 
-        # prepare device values
-        self._prepare_devices()
+        # prepare device parameter values
+        self._prepare_parameters()
 
-    def _prepare_devices(self):
-        self.freq_ftw0 = self.urukul0_ch0.frequency_to_ftw(self.frequency_mhz * MHz)
-        self.freq_ftw1 = self.urukul0_ch0.frequency_to_ftw(self.frequency_mhz * MHz)
+    def _prepare_parameters(self):
+        # calculate base waveform values
+        self.ampl_asf0 =                    self.urukul0_ch0.amplitude_to_asf(self.amplitude_pct / 100.)
+        self.ampl_asf1 =                    self.urukul0_ch0.amplitude_to_asf(self.amplitude_pct / 100.)
 
-        self.phase_pow0 = self.urukul0_ch0.turns_to_pow(self.phase_turns0)
-        self.phase_pow1 = self.urukul0_ch0.turns_to_pow(self.phase_turns1)
+        self.freq_ftw0 =                    self.urukul0_ch0.frequency_to_ftw(self.frequency_mhz * MHz)
+        self.freq_ftw1 =                    self.urukul0_ch0.frequency_to_ftw(self.frequency_mhz * MHz)
 
-        self.ampl_asf0 = self.urukul0_ch0.amplitude_to_asf(self.amplitude_pct / 100.)
-        self.ampl_asf1 = self.urukul0_ch0.amplitude_to_asf(self.amplitude_pct / 100.)
+        self.phase_ch0_offset_pow =         self.urukul0_ch0.turns_to_pow(self.phase_turns0)
+        self.phase_ch1_offset_pow =         self.urukul0_ch0.turns_to_pow(self.phase_turns1)
 
-        self.time_delay_mu = self.core.seconds_to_mu(self.time_delay_ns * ns)
-        self.time_phase_clear_mu = self.core.seconds_to_mu(750 * ns)
+
+        # calculate phase values to compensate for ch0 & ch1 delays
+        self.time_ch1_delay_ns =            1.66
+        self.phase_ch1_inherent_turns =     0.
+        self.phase_ch1_delay_turns =        (self.frequency_mhz * MHz) * (self.time_ch1_delay_ns * ns)
+        # combine compensation values into ch1 phase: ch0 vs ch1 inherent phase shift/relation, ch0 vs ch1 inherent time delay, and ch1 offset
+        self.phase_ch1_final_pow =          self.urukul0_ch0.turns_to_pow(self.phase_ch1_inherent_turns + self.phase_ch1_delay_turns + self.phase_turns1)
+
+
+        # calculate timings
+        self.time_delay_mu =                self.core.seconds_to_mu(self.time_delay_ns * ns)
+        self.time_phase_clear_mu =          self.core.seconds_to_mu(750 * ns)
+
+        # tmp remove
+        self.res00 = np.int32(0)
+        self.res10 = np.int32(0)
+
+        self.res01 = np.int32(0)
+        self.res11 = np.int32(0)
+        # tmp remove
 
     @kernel(flags={"fast-math"})
     def _run_prepare(self):
-        # prepare - TTLs
-        self.ttl8.off()
-        self.ttl9.off()
+        # prepare asynchronous/timing-agnostic stuff
+        with parallel:
+            # prepare - TTLs
+            self.ttl8.off()
+            self.ttl9.off()
 
-        # prepare - DDSs
-        self.urukul0_ch3.set_phase_mode(PHASE_MODE_ABSOLUTE)
-        self.urukul1_ch3.set_phase_mode(PHASE_MODE_ABSOLUTE)
-        self.core.break_realtime()
+            # prepare - DDSs
+            self.urukul0_ch3.set_phase_mode(PHASE_MODE_ABSOLUTE)
+            self.urukul1_ch3.set_phase_mode(PHASE_MODE_ABSOLUTE)
 
-        self.urukul0_ch3.set_att(14 * dB)
-        self.urukul1_ch3.set_att(14 * dB)
-        self.core.break_realtime()
+            self.urukul0_ch3.set_att(14 * dB)
+            self.urukul1_ch3.set_att(14 * dB)
 
-        # prepare - profiles
-        self.urukul0_ch3.set_mu(self.freq_ftw0, asf=0x01, pow_=0x0, profile=0)
-        self.urukul1_ch3.set_mu(self.freq_ftw1, asf=0x01, pow_=0x0, profile=0)
-        self.core.break_realtime()
-
-        self.urukul0_ch3.set_mu(self.freq_ftw0, asf=self.ampl_asf0, pow_=self.phase_pow0, profile=1)
-        self.urukul1_ch3.set_mu(self.freq_ftw1, asf=self.ampl_asf1, pow_=self.phase_pow1, profile=1)
-        self.core.break_realtime()
+        # prepare - DDS profiles
+        at_mu(now_mu() + 50000)
+        with parallel:
+            self.res00 = self.urukul0_ch3.set_mu(self.freq_ftw0, asf=0x01, pow_=0x0, profile=0)
+            self.res10 = self.urukul1_ch3.set_mu(self.freq_ftw1, asf=0x01, pow_=0x0, profile=0)
+        with parallel:
+            self.res01 = self.urukul0_ch3.set_mu(self.freq_ftw0, asf=self.ampl_asf0, pow_=self.phase_ch0_offset_pow, profile=1)
+            self.res11 = self.urukul1_ch3.set_mu(self.freq_ftw1, asf=self.ampl_asf1, pow_=self.phase_ch1_final_pow, profile=1)
 
         # set up registers
-        self.urukul0_ch3.set_cfr1(phase_autoclear=1)
-        self.urukul1_ch3.set_cfr1(phase_autoclear=1)
-
-        self.urukul0_ch3.set_cfr2(matched_latency_enable=1)
-        self.urukul1_ch3.set_cfr2(matched_latency_enable=1)
-        self.core.break_realtime()
+        at_mu(now_mu() + 50000)
+        self.set_cfr1_rdx(phase_autoclear=1, select_sine_output=1)
+        # with parallel:
+        #     self.urukul0_ch3.set_cfr1(phase_autoclear=1)
+        #     self.urukul1_ch3.set_cfr1(phase_autoclear=1)
+        with parallel:
+            self.urukul0_ch3.set_cfr2(matched_latency_enable=1)
+            self.urukul1_ch3.set_cfr2(matched_latency_enable=1)
 
         # set up for running
-        self.urukul0_cpld.set_profile(0)
-        self.urukul1_cpld.set_profile(0)
-        self.core.break_realtime()
-
-        self.urukul0_cpld.cfg_switches(0b1000)
-        self.urukul1_cpld.cfg_switches(0b1000)
-        self.core.break_realtime()
+        at_mu(now_mu() + 50000)
+        with parallel:
+            self.urukul0_cpld.set_profile(0)
+            self.urukul1_cpld.set_profile(0)
+        with parallel:
+            self.urukul0_cpld.cfg_switches(0b1000)
+            self.urukul1_cpld.cfg_switches(0b1000)
 
     @kernel(flags={"fast-math"})
     def run(self):
@@ -119,8 +140,11 @@ class testidkurukul(EnvExperiment):
         self._run_prepare()
 
         # prepare time
-        delay_mu(1000000)
-        time_start_mu = now_mu()
+        # note: changing this delay causes relative phase shift; seems to follow according to frequency period
+        # note: actually, seems to have more to do with SYNC_CLK/fixed timing periods
+        delay_mu(125000)
+        # note: we coarse align to previous SYNC_CLK period
+        time_start_mu = now_mu() & ~7
 
         # RUN
         # at_mu(time_start_mu)
@@ -133,19 +157,47 @@ class testidkurukul(EnvExperiment):
         #         self.urukul1_ch3.set_cfr1(phase_autoclear=1)
         #         self.urukul1_cpld.io_update.pulse_mu(8)
 
-        at_mu(time_start_mu + self.time_phase_clear_mu)
+        # start first waveform
+        at_mu(time_start_mu)
         with parallel:
             self.urukul0_cpld.set_profile(1)
+            # self.urukul1_cpld.set_profile(1)
 
             with sequential:
-                delay_mu(460)
+                delay_mu(475)
                 self.ttl8.on()
 
-
-        at_mu(time_start_mu + self.time_phase_clear_mu + self.time_delay_mu)
+        # start cancellation waveform
+        # at_mu(time_start_mu + self.time_delay_mu)
+        at_mu((time_start_mu + self.time_delay_mu) & ~0x3)
         with parallel:
-            self.ttl9.on()
             self.urukul1_cpld.set_profile(1)
+
+            with sequential:
+                delay_mu(475)
+                self.ttl9.on()
+
+        # at_mu(time_start_mu + 512)
+        # with parallel:
+        #     with sequential:
+        #         self.urukul0_ch3.set_cfr1(phase_autoclear=1)
+        #         self.urukul0_cpld.io_update.pulse_mu(8)
+        #
+        #     with sequential:
+        #         delay_mu(787)
+        #         self.ttl9.on()
+
+
+        # # at_mu((time_start_mu + 508) & ~0x7)
+        # at_mu((time_start_mu + 508))
+        # with parallel:
+        #     self.urukul0_cpld.io_update.pulse_mu(8)
+        #
+        #     with sequential:
+        #         delay_mu(84)
+        #         self.ttl9.on()
+
+
 
         # cleanup
         # with parallel:
@@ -157,8 +209,78 @@ class testidkurukul(EnvExperiment):
         with parallel:
             self.ttl8.off()
             self.ttl9.off()
-        self.core.break_realtime()
+        self.core.reset()
 
+
+    @kernel(flags={"fast-math"})
+    def set_cfr1_rdx(self,
+                 power_down: TInt32 = 0b0000,
+                 phase_autoclear: TInt32 = 0,
+                 drg_load_lrr: TInt32 = 0,
+                 drg_autoclear: TInt32 = 0,
+                 phase_clear: TInt32 = 0,
+                 internal_profile: TInt32 = 0,
+                 ram_destination: TInt32 = 0,
+                 ram_enable: TInt32 = 0,
+                 manual_osk_external: TInt32 = 0,
+                 osk_enable: TInt32 = 0,
+                 select_auto_osk: TInt32 = 0,
+                 select_sine_output: TInt32 = 1):
+        """Set CFR1. See the AD9910 datasheet for parameter meanings.
+
+        This method does not pulse IO_UPDATE.
+
+        :param power_down: Power down bits.
+        :param phase_autoclear: Autoclear phase accumulator.
+        :param phase_clear: Asynchronous, static reset of the phase accumulator.
+        :param drg_load_lrr: Load digital ramp generator LRR.
+        :param drg_autoclear: Autoclear digital ramp generator.
+        :param internal_profile: Internal profile control.
+        :param ram_destination: RAM destination
+            (:const:`RAM_DEST_FTW`, :const:`RAM_DEST_POW`,
+            :const:`RAM_DEST_ASF`, :const:`RAM_DEST_POWASF`).
+        :param ram_enable: RAM mode enable.
+        :param manual_osk_external: Enable OSK pin control in manual OSK mode.
+        :param osk_enable: Enable OSK mode.
+        :param select_auto_osk: Select manual or automatic OSK mode.
+        """
+        with parallel:
+            self.urukul0_ch3.write32(_AD9910_REG_CFR1,
+                         (ram_enable << 31) |
+                         (ram_destination << 29) |
+                         (manual_osk_external << 23) |
+                         (internal_profile << 17) |
+                         (select_sine_output << 16) |
+                         (drg_load_lrr << 15) |
+                         (drg_autoclear << 14) |
+                         (phase_autoclear << 13) |
+                         (phase_clear << 11) |
+                         (osk_enable << 9) |
+                         (select_auto_osk << 8) |
+                         (power_down << 4) |
+                         2)  # SDIO input only, MSB first
+            self.urukul1_ch3.write32(_AD9910_REG_CFR1,
+                         (ram_enable << 31) |
+                         (ram_destination << 29) |
+                         (manual_osk_external << 23) |
+                         (internal_profile << 17) |
+                         (select_sine_output << 16) |
+                         (drg_load_lrr << 15) |
+                         (drg_autoclear << 14) |
+                         (phase_autoclear << 13) |
+                         (phase_clear << 11) |
+                         (osk_enable << 9) |
+                         (select_auto_osk << 8) |
+                         (power_down << 4) |
+                         2)  # SDIO input only, MSB first
 
     def analyze(self):
-        pass
+        print('\n\tresults:')
+        print('\t\turukul0 profile 0 phase: {:.4f}'.format(self.urukul0_ch0.pow_to_turns(self.res00)))
+        print('\t\turukul1 profile 0 phase: {:.4f}'.format(self.urukul0_ch0.pow_to_turns(self.res10)))
+        print('\n')
+        print('\t\turukul0 profile 1 phase: {:.4f}'.format(self.urukul0_ch0.pow_to_turns(self.res01)))
+        print('\t\turukul1 profile 1 phase: {:.4f}'.format(self.urukul0_ch0.pow_to_turns(self.res11)))
+        print('\n')
+
+        print('idk: {}'.format(self.urukul0_ch3.sync_data.io_update_delay))
