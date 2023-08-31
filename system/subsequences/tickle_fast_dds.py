ea28d4d2c232008373706113291dcbea7c5b1e78
Clayton Ho <claytonho@g.ucla.edu>
2023-08-31 02:04:30 -0700

    removed fast switching cancel temporarily

--- system/subsequences/tickle_fast_dds.py
+++ system/subsequences/tickle_fast_dds.py
@@ -87,8 +87,10 @@ class TickleFastDDS(LAXSubsequence):
         at_mu(time_start_mu + self.time_system_prepare_delay_mu)
         self.dds_ch0.cpld.set_profile(1)
         # cancel output
-        at_mu(time_start_mu + self.time_system_prepare_delay_mu + self.time_delay_mu)
-        self.dds_ch1.cpld.set_profile(1)
+        # tmp remove undo
+        # at_mu(time_start_mu + self.time_system_prepare_delay_mu + self.time_delay_mu)
+        # self.dds_ch1.cpld.set_profile(1)
+        # tmp remove undo
 
         # tmp remove
         at_mu(time_start_mu + self.time_system_prepare_delay_mu + 475)
