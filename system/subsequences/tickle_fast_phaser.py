9887784a788994f336dadb4c287f2731f4d62c33
Clayton Ho <claytonho@g.ucla.edu>
2023-08-30 18:33:02 -0700

    new qlms rabi with phaser tickle doesn't give errors; haven't tested it in hardware yet

--- system/subsequences/tickle_fast_phaser.py
+++ system/subsequences/tickle_fast_phaser.py
@@ -16,7 +16,7 @@ class TickleFastPhaser(LAXSubsequence):
 
 
     def build_subsequence(self):
-        self.setattr_argument('att_ticklefast_phaser_db', NumberValue(default=24, ndecimals=1, step=0.5, min=0, max=31.5), group='ticklefast')
+        self.setattr_argument('att_ticklefast_phaser_db', NumberValue(default=5, ndecimals=1, step=0.5, min=0, max=31.5), group='ticklefast')
 
         # get relevant devices
         self.setattr_device('phaser_eggs')
