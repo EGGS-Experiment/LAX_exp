32a21941267f709b6555180483bba2107b798519
Clayton Ho <claytonho@g.ucla.edu>
2023-09-12 00:31:18 -0700

    set the correct value for phase_autoclear timings in extension/constants.py assuming that we use it in delay_mu() instead of at_mu() with reference to some start time

--- extensions/constants.py
+++ extensions/constants.py
@@ -8,8 +8,9 @@ __all__ = ['TIME_PROFILESWITCH_DELAY_MU', 'TIME_PHASEAUTOCLEAR_DELAY_MU', 'TIME_
 
 
 # urukul AD9910 timings
+# todo: change profileswitch timings to reflect actual delay between end of cfg_write and actual update; should be ~79ns just like for phase autoclear
 TIME_PROFILESWITCH_DELAY_MU =   420
-TIME_PHASEAUTOCLEAR_DELAY_MU =  787
+TIME_PHASEAUTOCLEAR_DELAY_MU =  78
 TIME_RFSWITCH_DELAY_MU =        31
 
 # todo: add ARTIQ phaser timings as well
