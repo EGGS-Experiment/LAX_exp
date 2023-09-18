31d40e4ed71dbcd7f414e5704c60a59294fed1f3
Clayton Ho <claytonho@g.ucla.edu>
2023-09-18 16:52:07 -0700

    fixed another bug in migration of squeezing to new sbc-type experiment structure

--- experiments/motional/Squeezing.py
+++ experiments/motional/Squeezing.py
@@ -151,7 +151,7 @@ class Squeezing(SidebandCooling.SidebandCooling):
                 '''READOUT'''
                 # set readout waveform for qubit
                 self.qubit.set_profile(0)
-                self.qubit.set_att_mu(self.att_readout_mu)
+                self.qubit.set_att_mu(self.sidebandreadout_subsequence.att_sideband_readout_mu)
                 # transfer population to D-5/2 state
                 self.qubit.on()
                 delay_mu(time_readout_mu)
