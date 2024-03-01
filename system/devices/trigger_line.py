b49d11ba44fad1b16fe748759184f2ff3cd7779c
Clayton Ho <claytonho@g.ucla.edu>
2024-02-29 23:26:01 -0800

    updated line trigger to have similar behavior to rf trigger; untested

--- system/devices/trigger_line.py
+++ system/devices/trigger_line.py
@@ -6,13 +6,13 @@ from LAX_exp.base import LAXDevice
 
 class TriggerLine(LAXDevice):
     """
-    Device: Line Trigger
+    Device: Trigger Line
 
     Triggers an event off the AC line.
     Requires the AC line to already be converted into a TTL signal.
     """
     name = "trigger_line"
-    core_device = ('trigger', 'ttl5')
+    core_device = ('ttl_input', 'ttl5')
     kernel_invariants = {
         "time_timeout_mu",
         "time_holdoff_mu"
@@ -24,41 +24,31 @@ class TriggerLine(LAXDevice):
         self.time_holdoff_mu = self.get_parameter('time_holdoff_us', group='linetrigger', override=False, conversion_function=seconds_to_mu, units=us)
 
     @kernel(flags={"fast-math"})
-    def trigger(self):
-    # def trigger(self, time_gating_mu: TInt64, time_holdoff_mu: TInt64) -> TInt64:
+    def trigger(self, time_gating_mu: TInt64 = self.time_timeout_mu, time_holdoff_mu: TInt64 = self.time_holdoff_mu) -> TInt64:
         """
-        Block until either the timeout period is reached, or a TTL edge is detected.
+        Trigger off a rising edge of the AC line.
+        Times out if no edges are detected.
+        Arguments:
+            time_gating_mu  (int)   : the maximum waiting time (in machine units) for the trigger signal.
+            time_holdoff_mu (int64) : the holdoff time (in machine units)
+        Returns:
+                            (int64) : the input time of the trigger signal.
         """
-        self.core.break_realtime()
+        # wait for line trigger input
+        time_trigger_mu = self.ttl_input.timestamp_mu(self.ttl_input.gate_rising_mu(time_gating_mu))
 
-        # wait for trigger input
-        time_trigger_mu = self.counter_trigger.timestamp_mu(self.trigger.gate_rising_mu(self.time_timeout_mu))
-
-        # respond to trigger input
+        # ensure input timestamp is valid
         if time_trigger_mu > 0:
-
             # set rtio hardware time to input trigger time
-            at_mu(time_trigger_mu + self.time_holdoff_mu)
+            at_mu(time_trigger_mu + time_holdoff_mu)
             return time_trigger_mu
 
-        # if no input detected, return -1
+        # reset RTIO if we don't get receive trigger signal for some reason
+        else:
+            # add slack before resetting system
+            self.core.break_realtime()
+            self.ttl_input._set_sensitivity(0)
+            self.core.reset()
+
+        # return -1 if we time out
         return -1
-        # # trigger sequence off same phase of RF
-        # self.input.gate_rising_mu(time_gating_mu)
-        # time_trigger_mu = self.input.timestamp_mu(now_mu())
-        #
-        # # ensure input timestamp is valid
-        # if time_trigger_mu >= 0:
-        #
-        #     # activate modulation and enable photon counting
-        #     at_mu(time_trigger_mu + time_holdoff_mu)
-        #     return now_mu()
-        #
-        # # reset RTIO if we don't get receive trigger signal for some reason
-        # else:
-        #     # add holdoff delay before resetting system
-        #     self.core.break_realtime()
-        #     self.rf_clock._set_sensitivity(0)
-        #     self.core.reset()
-        #
-        # return -1
--- system/devices/trigger_rf.py
+++ system/devices/trigger_rf.py
@@ -6,7 +6,7 @@ from LAX_exp.base import LAXDevice
 
 class TriggerRF(LAXDevice):
     """
-    Device: RF Trigger
+    Device: Trigger RF
 
     Wrapper for the ttl_input object that reads in the RF synchronization signal.
     """
@@ -16,7 +16,6 @@ class TriggerRF(LAXDevice):
 
     def prepare_device(self):
         pass
-        # self.rf_gating_edge =               self.get_parameter('rf_gating_edge', group='rf', override=False)
 
     @kernel(flags={"fast-math"})
     def trigger(self, time_gating_mu: TInt64, time_holdoff_mu: TInt64) -> TInt64:
@@ -35,14 +34,13 @@ class TriggerRF(LAXDevice):
 
         # ensure input timestamp is valid
         if time_trigger_mu >= 0:
-
             # activate modulation and enable photon counting
             at_mu(time_trigger_mu + time_holdoff_mu)
             return now_mu()
 
         # reset RTIO if we don't get receive trigger signal for some reason
         else:
-            # add holdoff delay before resetting system
+            # add slack before resetting system
             self.core.break_realtime()
             self.ttl_input._set_sensitivity(0)
             self.core.reset()
