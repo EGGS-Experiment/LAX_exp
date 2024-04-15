33c988c79939a992b0be2d1fcade3582089cfd80
Clayton Ho <claytonho@g.ucla.edu>
2024-04-14 20:14:41 -0700

    fixed trigger timing in phaser set and added a reset delay between pulses

--- utilities/phaser_set.py
+++ utilities/phaser_set.py
@@ -12,33 +12,34 @@ class PhaserSet(EnvExperiment):
 
     def build(self):
         # global arguments
-        self.setattr_argument("repetitions",                    NumberValue(default=5, ndecimals=0, step=1, min=1, max=10000))
-        self.setattr_argument("cleanup",                        BooleanValue(default=False), group='global')
+        self.setattr_argument("repetitions",                    NumberValue(default=100, ndecimals=0, step=1, min=1, max=10000))
+        self.setattr_argument("cleanup",                        BooleanValue(default=True), group='global')
         self.setattr_argument("freq_carrier_mhz",               NumberValue(default=82, ndecimals=5, step=1, min=1, max=10000), group='global')
-        self.setattr_argument("time_pulse_ms",                  NumberValue(default=0.25, ndecimals=5, step=1, min=0.000001, max=100000), group='global')
+        self.setattr_argument("time_pulse_ms",                  NumberValue(default=5., ndecimals=5, step=1, min=0.000001, max=100000), group='global')
+        self.setattr_argument("time_reset_ms",                  NumberValue(default=2., ndecimals=5, step=1, min=0.000001, max=100000), group='global')
         # self.setattr_argument("clear_dac_phase_accumulator", BooleanValue(default=False), group='global')
         self.setattr_argument("synchronize_oscillator_updates", BooleanValue(default=True), group='global')
 
         # channel-specific arguments
-        self.setattr_argument("att_ch0_db",                     NumberValue(default=8., ndecimals=1, step=0.5, min=0, max=31.5), group='channels')
-        self.setattr_argument("att_ch1_db",                     NumberValue(default=8., ndecimals=1, step=0.5, min=0, max=31.5), group='channels')
+        self.setattr_argument("att_ch0_db",                     NumberValue(default=3., ndecimals=1, step=0.5, min=0, max=31.5), group='channels')
+        self.setattr_argument("att_ch1_db",                     NumberValue(default=3., ndecimals=1, step=0.5, min=0, max=31.5), group='channels')
         self.setattr_argument("time_latency_ch1_system_ns",     NumberValue(default=0.17, ndecimals=3, step=0.1, min=-1.0, max=1.0), group='channels')
-        self.setattr_argument("apply_ch1_latency_on_oscillators", BooleanValue(default=False), group='channels')
-        self.setattr_argument("phase_inherent_ch1_turns",       NumberValue(default=0., ndecimals=3, step=0.1, min=-1.0, max=1.0), group='channels')
+        self.setattr_argument("apply_ch1_latency_on_oscillators", BooleanValue(default=True), group='channels')
+        self.setattr_argument("phase_inherent_ch1_turns",       NumberValue(default=0.07, ndecimals=3, step=0.1, min=-1.0, max=1.0), group='channels')
 
         # oscillators - channel 0
         # note: oscillator waveforms are in the format [freq_khz, ampl_pct, phas_turns]
-        self.setattr_argument("waveform_ch0_osc0",  PYONValue(default=[-767.5, 40., 0.475]), group='oscillators_ch0')
-        self.setattr_argument("waveform_ch0_osc1",  PYONValue(default=[767.5, 40., 0.25]), group='oscillators_ch0')
-        self.setattr_argument("waveform_ch0_osc2",  PYONValue(default=[0., 10., 0.]), group='oscillators_ch0')
+        self.setattr_argument("waveform_ch0_osc0",  PYONValue(default=[0., 98., 0.]), group='oscillators_ch0')
+        self.setattr_argument("waveform_ch0_osc1",  PYONValue(default=[0., 0., 0.]), group='oscillators_ch0')
+        self.setattr_argument("waveform_ch0_osc2",  PYONValue(default=[0., 0., 0.]), group='oscillators_ch0')
         self.setattr_argument("waveform_ch0_osc3",  PYONValue(default=[0., 0., 0.]), group='oscillators_ch0')
         self.setattr_argument("waveform_ch0_osc4",  PYONValue(default=[0., 0., 0.]), group='oscillators_ch0')
 
         # oscillators - channel 1
         # note: oscillator waveforms are in the format [freq_khz, ampl_pct, phas_turns]
-        self.setattr_argument("waveform_ch1_osc0",  PYONValue(default=[767.5, 40., 0.475]), group='oscillators_ch1')
-        self.setattr_argument("waveform_ch1_osc1",  PYONValue(default=[-767.5, 40., 0.25]), group='oscillators_ch1')
-        self.setattr_argument("waveform_ch1_osc2",  PYONValue(default=[0., 10., 0.5]), group='oscillators_ch1')
+        self.setattr_argument("waveform_ch1_osc0",  PYONValue(default=[0., 98., 0.]), group='oscillators_ch1')
+        self.setattr_argument("waveform_ch1_osc1",  PYONValue(default=[0., 0., 0.]), group='oscillators_ch1')
+        self.setattr_argument("waveform_ch1_osc2",  PYONValue(default=[0., 0., 0.]), group='oscillators_ch1')
         self.setattr_argument("waveform_ch1_osc3",  PYONValue(default=[0., 0., 0.]), group='oscillators_ch1')
         self.setattr_argument("waveform_ch1_osc4",  PYONValue(default=[0., 0., 0.]), group='oscillators_ch1')
 
@@ -90,13 +91,15 @@ class PhaserSet(EnvExperiment):
 
         ### TIMING ###
         # todo: double check this is OK and reproduces normal behavior
-        self.time_pulse_mu =                        self.core.seconds_to_mu(self.time_pulse_ms * ms)
+        self.time_pulse_mu =        self.core.seconds_to_mu(self.time_pulse_ms * ms)
         if self.time_pulse_mu % self.t_frame_mu:
-            t_frame_multiples =                     round(self.time_pulse_mu / self.t_frame_mu + 0.5)
-            self.time_pulse_mu =                    np.int64(self.t_frame_mu * t_frame_multiples)
+            t_frame_multiples =     round(self.time_pulse_mu / self.t_frame_mu + 0.5)
+            self.time_pulse_mu =    np.int64(self.t_frame_mu * t_frame_multiples)
+
+        self.time_reset_mu =        self.core.seconds_to_mu(self.time_reset_ms * ms)
 
         ### FREQUENCIES ###
-        self.freq_carrier_hz =                      self.freq_carrier_mhz * MHz
+        self.freq_carrier_hz =      self.freq_carrier_mhz * MHz
         self.freq_center_hz =       self.get_dataset('eggs.freq_center_mhz') * MHz
 
     def _prepare_oscillators(self):
@@ -167,6 +170,10 @@ class PhaserSet(EnvExperiment):
         self.dac_register_1f = self.phaser0.dac_read(0x1F)
         self.core.break_realtime()
 
+        # set debug triggers
+        self.ttl8.off()
+        self.ttl9.off()
+
     @kernel(flags={"fast-math"})
     def run(self):
         # PREPARE (HARDWARE)
@@ -182,6 +189,10 @@ class PhaserSet(EnvExperiment):
             self.phaser_reset()
             # set oscillator waveforms
             self.phaser_run()
+            # stop phaser
+            self.phaser_stop()
+            # add reset time
+            delay_mu(self.time_reset_mu)
 
         # CLEAN UP
         self.core.break_realtime()
@@ -204,7 +215,7 @@ class PhaserSet(EnvExperiment):
         Puts the same RSB and BSB on both channels, and sets a third oscillator to 0 Hz in case dynamical decoupling is used.
         """
         # calculate phase delays between CH0 and CH1
-        at_mu(now_mu() + 1000000)
+        self.core.break_realtime()
         self.phase_ch1_turns =          (self.phase_inherent_ch1_turns +
                                          (self.freq_carrier_hz * self.time_latency_ch1_system_ns * ns))
 
@@ -228,8 +239,7 @@ class PhaserSet(EnvExperiment):
         # oscillator 4
         self.phase_ch0_osc2 =           self.freq_ch0_osc_hz_list[4] * 4 * self.t_sample_mu * ns + self.phas_ch0_osc_turns_list[4]
         self.phase_ch1_osc2 =           self.freq_ch1_osc_hz_list[4] * 4 * self.t_sample_mu * ns + self.phas_ch1_osc_turns_list[4]
-        at_mu(now_mu() + 1000000)
-
+        self.core.break_realtime()
 
         # set carrier offset frequency via the DUC
         at_mu(self.phaser0.get_next_frame_mu())
@@ -333,6 +343,9 @@ class PhaserSet(EnvExperiment):
         """
         Stop the oscillators on phaser.
         """
+        # get synchronized start time
+        time_start_mu = self.phaser0.get_next_frame_mu()
+
         # set oscillator 0
         at_mu(time_start_mu)
         with parallel:
@@ -413,25 +426,26 @@ class PhaserSet(EnvExperiment):
         self.core.break_realtime()
 
     def analyze(self):
-        print("\tconfig:")
-        print("\tch1 global latency: {:.3f}\n".format(self.phase_ch1_turns))
-
-        print("\tosc0:")
-        print("\t\tphase ch0 osc0: {:.3f}\n".format(self.phase_ch0_osc0))
-        print("\t\tphase ch1 osc0: {:.3f}\n".format(self.phase_ch1_osc0))
-
-        print("\tosc1:")
-        print("\t\tphase ch0 osc1: {:.3f}".format(self.phase_ch0_osc1))
-        print("\t\tphase ch1 osc1: {:.3f}\n".format(self.phase_ch1_osc1))
-
-        print("\tosc2:")
-        print("\t\tphase ch0 osc2: {:.3f}".format(self.phase_ch0_osc2))
-        print("\t\tphase ch1 osc2: {:.3f}\n".format(self.phase_ch1_osc2))
-
-        print("\tosc3:")
-        print("\t\tphase ch0 osc3: {:.3f}".format(self.phase_ch0_osc3))
-        print("\t\tphase ch1 osc3: {:.3f}\n".format(self.phase_ch1_osc3))
-
-        print("\tosc4:")
-        print("\t\tphase ch0 osc4: {:.3f}".format(self.phase_ch0_osc4))
-        print("\t\tphase ch1 osc4: {:.3f}\n".format(self.phase_ch1_osc4))
+        pass
+        # print("\tconfig:")
+        # print("\tch1 global latency: {:.3f}\n".format(self.phase_ch1_turns))
+        #
+        # print("\tosc0:")
+        # print("\t\tphase ch0 osc0: {:.3f}\n".format(self.phase_ch0_osc0))
+        # print("\t\tphase ch1 osc0: {:.3f}\n".format(self.phase_ch1_osc0))
+        #
+        # print("\tosc1:")
+        # print("\t\tphase ch0 osc1: {:.3f}".format(self.phase_ch0_osc1))
+        # print("\t\tphase ch1 osc1: {:.3f}\n".format(self.phase_ch1_osc1))
+        #
+        # print("\tosc2:")
+        # print("\t\tphase ch0 osc2: {:.3f}".format(self.phase_ch0_osc2))
+        # print("\t\tphase ch1 osc2: {:.3f}\n".format(self.phase_ch1_osc2))
+        #
+        # print("\tosc3:")
+        # print("\t\tphase ch0 osc3: {:.3f}".format(self.phase_ch0_osc3))
+        # print("\t\tphase ch1 osc3: {:.3f}\n".format(self.phase_ch1_osc3))
+        #
+        # print("\tosc4:")
+        # print("\t\tphase ch0 osc4: {:.3f}".format(self.phase_ch0_osc4))
+        # print("\t\tphase ch1 osc4: {:.3f}\n".format(self.phase_ch1_osc4))
