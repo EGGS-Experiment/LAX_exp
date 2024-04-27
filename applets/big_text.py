1b1e1b0c93b3a595833958e90bfedae1ba5b6fa5
Clayton Ho <claytonho@g.ucla.edu>
2024-04-26 17:21:29 -0700

    major updates to big_text

--- applets/big_text.py
+++ applets/big_text.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
+import os
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
 from artiq.applets.simple import SimpleApplet
 
-from playsound import playsound
-
-# _PLAYSOUND_ENABLE = False
-# try:
-#     # todo: try to make resource imports programmatic
-#     #from inspect import getfile, getmodule
-#     from playsound import playsound
-#     from twisted.internet.threads import deferToThread
-#     _UNLOCKED_SOUND_PATH = os.path.join(os.environ['EGGS_LABRAD_ROOT'], 'EGGS_labrad\\clients\\wavemeter_client\\channel_unlocked_short.mp3')
-#     _PLAYSOUND_ENABLE = True
-# except Exception as e:
-#     print('Error: playsound package not installed.')
+# set up audio warning
+_PLAYSOUND_ENABLE = False
+try:
+    from playsound import playsound
+    _SOUND_PATH_WARNING = os.path.join(
+        os.environ['EGGS_LABRAD_ROOT'],
+        'EGGS_labrad\\clients\\wavemeter_client\\channel_unlocked_short.mp3'
+    )
+    _PLAYSOUND_ENABLE = True
+except Exception as e:
+    print('Warning: playsound package not installed.')
 
 
 class TextWidget(QtWidgets.QLabel):
@@ -30,6 +30,10 @@ class TextWidget(QtWidgets.QLabel):
         self.setFont(QFont('MS Shell Dlg 2', pointSize=19))
         self.setAlignment(Qt.AlignCenter | Qt.AlignHCenter)
 
+        # play sound to test
+        if _PLAYSOUND_ENABLE:
+            playsound(_SOUND_PATH_WARNING)
+
 
     def data_changed(self, data, mods):
         try:
@@ -39,7 +43,10 @@ class TextWidget(QtWidgets.QLabel):
             # configure error display
             if 'ERR' in text.split(':')[0].upper():
                 self.setStyleSheet('background-color: red; color: white')
-                # todo: play sound
+                # play sound to warn sers
+                if _PLAYSOUND_ENABLE:
+                    for i in range(3):
+                        playsound(_SOUND_PATH_WARNING)
 
             # configure display for normal operation
             else:
