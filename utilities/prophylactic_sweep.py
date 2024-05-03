57766a701b118ccea1b5f8941cd0aebaf8cf98ea
Clayton Ho <claytonho@g.ucla.edu>
2024-05-03 00:31:06 -0700

    fixed lots more bugs in prophylactic sweep; now works

--- utilities/prophylactic_sweep.py
+++ utilities/prophylactic_sweep.py
@@ -12,33 +12,35 @@ class ProphylacticSweep(EnvExperiment):
 
 
     def build(self):
+        # get core devices
         self.setattr_device("core")
         self.setattr_device("core_dma")
         self.setattr_device("scheduler")
 
         # modulation
-        self.setattr_argument("mod_time_total_s",                   NumberValue(default=10, ndecimals=3, step=1, min=0.001, max=10000000))
-        self.setattr_argument("mod_att_db",                         NumberValue(default=31, ndecimals=1, step=0.5, min=0., max=31.5))
-        self.setattr_argument("mod_freq_khz_list",                  Scannable(
-                                                                        default=CenterScan(1686, 10, 1., randomize=True),
-                                                                        global_min=1, global_max=100000, global_step=1.,
-                                                                        unit="kHz", scale=1, ndecimals=3
-                                                                    ))
+        self.setattr_argument("repetitions",            NumberValue(default=5, ndecimals=0, step=1, min=1, max=10000))
+        self.setattr_argument("mod_time_total_s",       NumberValue(default=20, ndecimals=3, step=1, min=0.001, max=10000000))
+        self.setattr_argument("mod_att_db",             NumberValue(default=3, ndecimals=1, step=0.5, min=0., max=31.5))
+        self.setattr_argument("mod_freq_khz_list",      Scannable(
+                                                            default=CenterScan(1686, 1000, 100., randomize=True),
+                                                            global_min=1, global_max=100000, global_step=1.,
+                                                            unit="kHz", scale=1, ndecimals=3
+                                                        ))
 
     def prepare(self):
-        # modulation control
-        self.mod_dds =                                              self.get_device("urukul1_ch1")
-        self.mod_dds_sw =                                           self.get_device("ttl11")
-        self.mod_dds_ampl_pct =                                     self.mod_dds.amplitude_to_asf(0.35)
-        self.mod_dds_att_mu =                                       self.mod_dds.cpld.att_to_mu(self.mod_att_db * dB)
-        self.mod_freq_mu_list =                                     np.array([
-                                                                        self.mod_dds.frequency_to_ftw(freq_khz * kHz)
-                                                                        for freq_khz in self.mod_freq_khz_list
-                                                                    ])
-        self.mod_time_mu =                                          self.core.seconds_to_mu(self.mod_time_total_s / len(self.mod_freq_mu_list))
+        # modulation devices
+        self.mod_dds =                      self.get_device("urukul1_ch1")
+        self.mod_dds_sw =                   self.get_device("ttl11")
+        self.mod_dds_ampl_pct =             self.mod_dds.amplitude_to_asf(0.35)
+        self.mod_dds_att_mu =               self.mod_dds.cpld.att_to_mu(self.mod_att_db * dB)
 
-        # cooling holdoff time
-        self.time_cooling_holdoff_mu =                              self.core.seconds_to_mu(3 * ms)
+        # timing/iteration
+        self.mod_freq_mu_list =             np.array([
+                                                self.mod_dds.frequency_to_ftw(freq_khz * kHz)
+                                                for freq_khz in self.mod_freq_khz_list
+                                            ])
+        self.mod_time_mu =                  self.core.seconds_to_mu(self.mod_time_total_s / (len(self.mod_freq_mu_list) * self.repetitions))
+        self.time_cooling_holdoff_mu =      self.core.seconds_to_mu(3 * ms)
 
 
     @kernel(flags={"fast-math"})
@@ -52,36 +54,38 @@ class ProphylacticSweep(EnvExperiment):
 
 
         # MAIN LOOP
-        # sweep modulation frequency
-        for freq_mu in self.mod_freq_mu_list:
+        for i in range(self.repetitions):
+            # sweep modulation frequency
+            for freq_mu in self.mod_freq_mu_list:
 
-            # add holdoff period for recooling the ion
-            delay_mu(self.time_cooling_holdoff_mu)
+                # synchronize with timeline
+                self.core.wait_until_mu(now_mu())
+                self.core.break_realtime()
 
-            # set modulation frequency and tickle
-            self.mod_dds.set_mu(freq_mu, asf=self.mod_dds_ampl_pct)
-            self.mod_dds.sw.on()
-            self.mod_dds_sw.on()
+                # add holdoff period for recooling the ion
+                delay_mu(self.time_cooling_holdoff_mu)
 
-            # apply prophylaxis for given time
-            delay_mu(self.mod_time_mu)
+                # set modulation frequency and tickle
+                self.mod_dds.set_mu(freq_mu, asf=self.mod_dds_ampl_pct)
+                self.mod_dds.sw.on()
+                self.mod_dds_sw.on()
 
-            # turn off output
-            self.mod_dds.sw.off()
+                # apply prophylaxis for given time
+                delay_mu(self.mod_time_mu)
 
-            # check termination
-            if self.scheduler.check_termination():
-                raise TerminationRequested
+                # turn off output
+                self.mod_dds.sw.off()
 
-        # delay experiment cancellation until sequence has finished
-        self.core.wait_until_mu(now_mu())
-        self.core.break_realtime()
+                # todo: update progress bar
 
-        # clean up DDS to prevent leakage
-        self.mod_dds.set_att(31.5 * dB)
-        self.mod_dds.set_mu(0x01, asf=0x01)
-        self.mod_dds.sw.off()
-        self.mod_dds_sw.off()
+
+                # check termination
+                if self.scheduler.check_termination():
+                    self.cleanupDevices()
+                    return
+
+        # clean up devices
+        self.cleanupDevices()
 
 
     @kernel(flags={"fast-math"})
@@ -91,6 +95,26 @@ class ProphylacticSweep(EnvExperiment):
         """
         # configure rf modulation source
         self.mod_dds.sw.off()
-        self.mod_dds.cfg_sw(True)
         self.mod_dds.set_att_mu(self.mod_dds_att_mu)
         self.core.break_realtime()
+
+    @kernel(flags={"fast-math"})
+    def cleanupDevices(self):
+        """
+        Clean up devices.
+        """
+        # delay experiment cancellation until sequence has finished
+        self.core.reset()
+        self.core.break_realtime()
+
+        # clean up DDS to prevent leakage
+        self.mod_dds.set_att(31.5 * dB)
+        self.mod_dds.set_mu(0x01, asf=0x01)
+        # switch off all switches
+        self.mod_dds_sw.off()
+        self.mod_dds.cfg_sw(False)
+        self.mod_dds.sw.off()
+        self.core.break_realtime()
+
+        # wait until cleanup has completed
+        self.core.wait_until_mu(now_mu())
