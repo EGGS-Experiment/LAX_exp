364b4a755e79c023a14b5a578dcdefbe1d3a3303
Clayton Ho <claytonho@g.ucla.edu>
2024-06-11 14:00:46 -0700

    further fix to imag align

--- utilities/imaging_alignment.py
+++ utilities/imaging_alignment.py
@@ -55,7 +55,7 @@ class ImagingAlignment(LAXExperiment, Experiment):
 
         # prepare datasets for storing counts
         self.set_dataset('temp.imag_align.counts_x', np.zeros(self.repetitions) * np.nan, broadcast=True, persist=False, archive=False)
-        self.set_dataset('temp.imag_align.counts_y', np.zeros((self.repetitions, 3)) * np.zeros, broadcast=True, persist=False, archive=False)
+        self.set_dataset('temp.imag_align.counts_y', np.zeros((self.repetitions, 3)), broadcast=True, persist=False, archive=False)
 
     @property
     def results_shape(self):
