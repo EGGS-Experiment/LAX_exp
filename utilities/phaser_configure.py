35a3fc995805f3a83c73b406ed370fcb1baf6685
Clayton Ho <claytonho@g.ucla.edu>
2024-09-22 19:04:20 -0700

    bugfix attempt for phaser_configure

--- utilities/phaser_configure.py
+++ utilities/phaser_configure.py
@@ -50,9 +50,9 @@ class PhaserConfigure(EnvExperiment):
         self.setattr_device("core")
 
         # get list of valid phaser devices and set them as arguments
-        # phaser_device_list = self._get_phaser_devices()
-        # self.setattr_argument("phaser_target",      EnumerationValue(list(phaser_device_list), default='phaser0'))
-        self.setattr_argument("phaser_target",      EnumerationValue(['phaser0', 'phaser1'], default='phaser0'))
+        phaser_device_list = self._get_phaser_devices()
+        self.setattr_argument("phaser_target",      EnumerationValue(list(phaser_device_list), default='phaser0'))
+        # self.setattr_argument("phaser_target",      EnumerationValue(['phaser0', 'phaser1'], default='phaser0'))
         #
         # # frequency configuration
         self.setattr_argument("freq_nco_mhz",       NumberValue(default=-217.083495, ndecimals=6, step=100, min=-400., max=400.))
