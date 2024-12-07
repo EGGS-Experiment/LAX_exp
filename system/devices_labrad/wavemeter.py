77c91ae74a7e102d164c8278afc28e11398e7bf5
Clayton Ho <claytonho@g.ucla.edu>
2024-12-06 17:56:15 -0800

    changes to wavemeter devices

--- system/devices_labrad/wavemeter.py
+++ system/devices_labrad/wavemeter.py
@@ -50,4 +50,9 @@ class Wavemeter(LAXDevice):
             channel (TInt32): wavemeter channel
             freq_thz: frequency in THz to set wavemeter PID to
         """
+
+        if channel == 5:
+            assert 755.21 freq_thz < 755.23, "Check Set Frequency of 397 Channel for Wavemeter"
+
+
         self.wavemeter.set_pid_course(channel, freq_thz)
