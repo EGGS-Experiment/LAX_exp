b8315e699973be89d5c7b5d779193f7183116fb5
Clayton Ho <claytonho@g.ucla.edu>
2025-02-06 01:22:35 -0800

    minor update to fft analysis

--- tools/fft_analyzer_trace.py
+++ tools/fft_analyzer_trace.py
@@ -13,17 +13,23 @@ class FFTAnalyzerTrace(EnvExperiment):
     """
 
     def build(self):
-        self.setattr_argument("device_key", StringValue(default="GPIB1::10"))
-        self.setattr_argument("config_string", StringValue(default="GPIB1::10"))
+        self.setattr_argument("device_key",         StringValue(default="GPIB1::10"))
+        self.setattr_argument("display_num",        NumberValue(default=0, precision=0, step=1, min=0, max=1))
+        self.setattr_argument("config_string",      StringValue(default="N/A"))
         self.setattr_argument("calibration_factor", NumberValue(default=5., precision=2, step=10, min=-100, max=100))
 
     def prepare(self):
         """
         todo: document
         """
+        # ensure display num is either 0 or 1
+        self.display_num = round(self.display_num)
+
         # set relevant parameters as dataset
         self.set_dataset('config', self.config_string, broadcast=False)
+        self.set_dataset('display_num', self.display_num, broadcast=False)
         self.set_dataset('calibration_factor', self.calibration_factor, broadcast=False)
+
 
     """
     PYVISA METHODS
@@ -47,19 +53,25 @@ class FFTAnalyzerTrace(EnvExperiment):
         self.dev.write_termination = '\n'   # default is '\r\n', which is invalid for SR7xx
         self.dev.timeout = 10000            # in ms, default is 3s
 
+        # todo: store important parameters
+        # todo: store PSD units?
+        # todo: store linear vs vector averaging
+        # todo: store units
+        # todo: store measurement type
+
     def get_trace(self) -> TNone:
         """
         Get 2D trace values
         """
         # get frequency start/stop
-        self.freq_start_hz =    float(self.dev.query("FSTR? 0").strip())
-        self.freq_stop_hz =     float(self.dev.query("FEND? 0").strip())
-        self.num_freq_points =  int(self.dev.query("DSPN? 0").strip())
+        self.freq_start_hz =    float(self.dev.query("FSTR? {:d}".format(self.display_num)).strip())
+        self.freq_stop_hz =     float(self.dev.query("FEND? {:d}".format(self.display_num)).strip())
+        self.num_freq_points =  int(self.dev.query("DSPN? {:d}".format(self.display_num)).strip())
         # create x-axis array in Hz
         xvals_freq_hz = np.linspace(self.freq_start_hz, self.freq_stop_hz, self.num_freq_points)
 
         # get trace values and convert to float (units should be dBm)
-        yvals_ampl_dbm = self.dev.query("DSPY? 0")
+        yvals_ampl_dbm = self.dev.query("DSPY? {:d}".format(self.display_num))
         yvals_ampl_dbm = np.array([float(str_val) for str_val in yvals_ampl_dbm.split(",")])
         # update results w/calibration factor
         yvals_ampl_dbm += self.calibration_factor
