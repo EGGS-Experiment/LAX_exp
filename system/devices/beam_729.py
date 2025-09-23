45fac8d4749e8ad84a2c9f673e1206ed38f34e27
Clayton Ho <claytonho@g.ucla.edu>
2025-09-23 00:01:25 -0700

    added error checking for heralding to CatStateCharacterize.py
    unified 729nm-related DDS control under qubit LAXDevice
    applied updates to CatStateCharacterize.py

--- system/devices/beam_729.py
+++ system/devices/beam_729.py
@@ -1,4 +1,5 @@
 from artiq.experiment import *
+from artiq.coredevice import ad9910
 from artiq.coredevice.urukul import DEFAULT_PROFILE
 
 from LAX_exp.extensions import *
@@ -14,40 +15,181 @@ class Beam729(LAXDevice):
     name = "qubit"
     core_device = ('beam', 'urukul0_ch0')
     devices = {
-        'rf_switch':    'ttl14'
+        'rf_switch':        'ttl14',
+        'singlepass0':      'urukul0_ch1',
+        'singlepass1':      'urukul0_ch2',
+        'doublepass_inj':   'urukul0_ch3',
     }
+
     kernel_invariants = {
+        # hardware devices
         "cpld", "sw",
-        "freq_qubit_ftw", "ampl_qubit_asf"
+
+        # DDS parameters - qubit/main/doublepass (chamber)
+        "freq_qubit_ftw", "ampl_qubit_asf",
+
+        # DDS parameters - singlepass0 (injection lock)
+        "freq_singlepass0_default_ftw", "ampl_singlepass0_default_asf", "att_singlepass0_default_mu",
+
+        # DDS parameters - singlepass1 (injection lock)
+        "freq_singlepass1_default_ftw", "ampl_singlepass1_default_asf", "att_singlepass1_default_mu",
+
+        # DDS parameters - doublepass (injection lock)
+        "freq_doublepass_inj_default_ftw", "ampl_doublepass_inj_default_asf",
+        "att_doublepass_inj_default_mu",
     }
 
     def prepare_device(self):
+        """
+        todo: document
+        """
         # re-alias relevant base devices
         self.sw =   self.beam.sw
         self.cpld = self.beam.cpld
 
-        # get beam parameters
+        # get main DDS (chamber doublepass) parameters
         self.freq_qubit_ftw = self.get_parameter('freq_qubit_mhz', group='beams.freq_mhz', override=False,
                                                  conversion_function=hz_to_ftw, units=MHz)
         self.ampl_qubit_asf = self.get_parameter('ampl_qubit_pct', group='beams.ampl_pct', override=False,
                                                  conversion_function=pct_to_asf)
 
+        # get singlepass (injection lock) parameters
+        self.freq_singlepass0_default_ftw = self.get_parameter('freq_729_singlepass0_mhz', group='beams.freq_mhz',
+                                                               override=False,
+                                                               conversion_function=hz_to_ftw, units=MHz)
+        self.freq_singlepass1_default_ftw = self.get_parameter('freq_729_singlepass1_mhz', group='beams.freq_mhz',
+                                                               override=False,
+                                                               conversion_function=hz_to_ftw, units=MHz)
+
+        self.ampl_singlepass0_default_asf = self.get_parameter('ampl_729_singlepass0_pct', group='beams.ampl_pct',
+                                                               override=False,
+                                                               conversion_function=pct_to_asf, units=MHz)
+        self.ampl_singlepass1_default_asf = self.get_parameter('ampl_729_singlepass1_pct', group='beams.ampl_pct',
+                                                               override=False,
+                                                               conversion_function=pct_to_asf, units=MHz)
+
+        self.att_singlepass0_default_mu = self.get_parameter('att_729_singlepass0_db', group='beams.att_db',
+                                                             override=False,
+                                                             conversion_function=att_to_mu, units=MHz)
+        self.att_singlepass1_default_mu = self.get_parameter('att_729_singlepass1_db', group='beams.att_db',
+                                                             override=False,
+                                                             conversion_function=att_to_mu, units=MHz)
+
+        # get doublepass (injection lock) parameters
+        self.freq_doublepass_inj_default_ftw =  self.get_parameter('freq_729_doublepass_inj_mhz',
+                                                                   group='beams.freq_mhz', override=False,
+                                                                   conversion_function=hz_to_ftw, units=MHz)
+        self.ampl_doublepass_inj_default_asf =  self.get_parameter('ampl_729_doublepass_inj_pct',
+                                                                   group='beams.ampl_pct', override=False,
+                                                                   conversion_function=pct_to_asf, units=MHz)
+        self.att_doublepass_inj_default_mu =    self.get_parameter('att_729_doublepass_inj_db',
+                                                                   group='beams.att_db', override=False,
+                                                                   conversion_function=att_to_mu, units=MHz)
+
+    def _prepare_device_values(self):
+        """
+        Check device parameters for validity.
+        """
+        # todo: check all ampls in [0, 50]
+        # todo: check singlepass atts in [7, 31.5]
+        # todo: check doublepass att in [6, 31.5]
+        pass
+
+
+    '''
+    LAXDEVICE METHODS
+    '''
     @kernel(flags={"fast-math"})
     def initialize_device(self) -> TNone:
+        """
+        todo: document
+        """
         # get CPLD attenuations so we don't override them
         self.cpld.get_att_mu()
         self.core.break_realtime()
 
-    @kernel(flags={"fast-math"})
-    def cleanup_device(self) -> TNone:
-        # set default profile on CPLD
-        self.set_profile(DEFAULT_PROFILE)
-        self.cpld.io_update.pulse_mu(8)
-        self.off()
-        delay_mu(10000)
+        # ensure phase_autoclear disabled on all beams to prevent phase accumulator reset
+        self.set_cfr1()
+        self.singlepass0.set_cfr1()
+        self.singlepass1.set_cfr1()
+        self.doublepass_inj.set_cfr1()
+        self.io_update()
+        delay_mu(25000)
+
+        # set up relevant AOMs to default values on ALL profiles
+        # necessary b/c not all AOMs are configured/used for each experiment
+        for i in range(8):
+            self.singlepass0.set_mu(self.freq_singlepass0_default_ftw,
+                                    asf=self.ampl_singlepass0_default_asf,
+                                    profile=i, phase_mode=ad9910.PHASE_MODE_CONTINUOUS)
+            delay_mu(8000)
+            self.singlepass1.set_mu(self.freq_singlepass1_default_ftw,
+                                    asf=self.ampl_singlepass1_default_asf,
+                                    profile=i, phase_mode=ad9910.PHASE_MODE_CONTINUOUS)
+            delay_mu(8000)
+            self.doublepass_inj.set_mu(self.freq_doublepass_inj_default_ftw,
+                                    asf=self.ampl_doublepass_inj_default_asf,
+                                    profile=i, phase_mode=ad9910.PHASE_MODE_CONTINUOUS)
+            delay_mu(8000)
+
+        # set AOMs for normal output/operation
+        self.singlepass0.set_att_mu(self.att_singlepass0_default_mu)
+        self.singlepass1.set_att_mu(self.att_singlepass1_default_mu)
+        self.doublepass_inj.set_att_mu(self.att_doublepass_inj_default_mu)
+        delay_mu(25000)
+
+        self.singlepass0.sw.on()
+        self.singlepass1.sw.off()
+        self.doublepass_inj.sw.on()
+        delay_mu(25000)
 
     @kernel(flags={"fast-math"})
+    def cleanup_device(self) -> TNone:
+        """
+        todo: document
+        """
+        # set up relevant AOMs to default values on ALL profiles
+        # necessary b/c not all AOMs are configured/used for each experiment
+        for i in range(8):
+            self.singlepass0.set_mu(self.freq_singlepass0_default_ftw,
+                                    asf=self.ampl_singlepass0_default_asf,
+                                    profile=i, phase_mode=ad9910.PHASE_MODE_CONTINUOUS)
+            delay_mu(8000)
+            self.singlepass1.set_mu(self.freq_singlepass1_default_ftw,
+                                    asf=self.ampl_singlepass1_default_asf,
+                                    profile=i, phase_mode=ad9910.PHASE_MODE_CONTINUOUS)
+            delay_mu(8000)
+            self.doublepass_inj.set_mu(self.freq_doublepass_inj_default_ftw,
+                                    asf=self.ampl_doublepass_inj_default_asf,
+                                    profile=i, phase_mode=ad9910.PHASE_MODE_CONTINUOUS)
+            delay_mu(8000)
+
+        # set AOMs for normal output/operation
+        self.singlepass0.set_att_mu(self.att_singlepass0_default_mu)
+        self.singlepass1.set_att_mu(self.att_singlepass1_default_mu)
+        self.doublepass_inj.set_att_mu(self.att_doublepass_inj_default_mu)
+        delay_mu(25000)
+
+        self.singlepass0.sw.on()
+        self.singlepass1.sw.off()
+        self.doublepass_inj.sw.on()
+        delay_mu(25000)
+
+        # return to default profile on CPLD (this is the default profile used by user/GUIs)
+        self.set_profile(DEFAULT_PROFILE)
+        self.io_update()
+        self.sw.off() # note: only turn off DDS int sw - leave ext sw OK for user
+        delay_mu(10000)
+
+
+    '''
+    HARDWARE METHODS
+    '''
+    @kernel(flags={"fast-math"})
     def on(self) -> TNone:
+        """
+        todo: document
+        """
         with parallel:
             # enable RF switch onboard Urukul
             self.sw.on()
@@ -59,6 +201,9 @@ class Beam729(LAXDevice):
 
     @kernel(flags={"fast-math"})
     def off(self) -> TNone:
+        """
+        todo: document
+        """
         with parallel:
             # disable RF switch onboard Urukul
             self.sw.off()
@@ -70,6 +215,13 @@ class Beam729(LAXDevice):
 
     @kernel(flags={"fast-math"})
     def set_profile(self, profile_num: TInt32) -> TNone:
+        """
+        todo: document
+        todo: do we really need this delay? shouldn't we hit io_update THEN delay?
+        maybe: we don't really want set_profile to pulse io_update b/c it might
+            fuck things up, e.g. clear phase accumulator
+        :param profile_num: the AD9910 profile number to set. Must be an int in [0, 7].
+        """
         self.cpld.set_profile(profile_num)
         delay_mu(TIME_AD9910_PROFILE_SWITCH_DELAY_MU)
 
--- system/subsequences/readout_adaptive.py
+++ system/subsequences/readout_adaptive.py
@@ -54,9 +54,17 @@ class ReadoutAdaptive(LAXSubsequence):
         """
         Prepare & precompute experimental values.
         """
-        '''SANITIZE INPUT'''
+        '''GENERAL SETUP/SANITIZE INPUT'''
         # timing/latencies become challenging below/near 5us bin times
-        if self.time_bin_us <= 5: raise ValueError("Invalid bin timing for ReadoutAdaptive. time_bin_us must be >5us.")
+        if self.time_bin_us <= 5:
+            raise ValueError("Invalid bin timing for ReadoutAdaptive. time_bin_us must be >5us.")
+
+        # print/log warning notifying user to set PMT count rates properly
+        print("\n\n"
+              "\tWarning: parent experiment uses ReadoutAdaptive, which requires Bright/Dark count rates be set.\n"
+              "\t\tEnsure sequences.adaptive_readout.count_rate_bright_3ms and "
+              "sequences.adaptive_readout.count_rate_dark_3ms are set in dataset manager."
+              "\n\n")
 
         '''PREPARE PARAMETERS'''
         time_readout_us =   self.get_parameter('time_readout_us', group='timing', override=False)
