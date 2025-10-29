384aa9dd95783adabd96e2df9f5f9e669ecc39bc
EGGS_lab <hudsonlabwifiweather1@gmail.com>
2025-10-28 19:11:19 -0700

    added tooltips to sampler_read.py

--- experiments/tools/sampler_read.py
+++ experiments/tools/sampler_read.py
@@ -1,13 +1,17 @@
 import numpy as np
 from artiq.experiment import *
 from artiq.coredevice.sampler import adc_mu_to_volt
+# todo: create a slower, vibes-based sampler read exp
+# todo: implement progress bar
 
 
-class SamplerRead(EnvExperiment):
+class SamplerReadFast(EnvExperiment):
     """
-    Tool: Sampler Read
+    Tool: Sampler Read Fast
 
     Read Sampler values over time.
+    Warning: due to timing concerns, this experiment CAN NOT be cancelled after submission
+        without loss of data.
     """
     kernel_invariants = {
         'adc', 'time_delay_mu', 'repetitions',
@@ -19,11 +23,21 @@ class SamplerRead(EnvExperiment):
         self.setattr_device('core')
 
         # channels & gains
-        self.setattr_argument("channel_gain_dict",  PYONValue({2: 100}))
+        self.setattr_argument("channel_gain_dict",  PYONValue({2: 100}),
+                              tooltip="A dictionary of {channel_number: gain_factor}.\n"
+                                      "Gain factor must be an int in [1, 10, 100, 1000].\n"
+                                      "Can record multiple channels, though this will limit the max sample rate (due to timing overheads).")
 
         # timing
-        self.setattr_argument("sample_rate_hz",     NumberValue(default=5000, precision=3, step=1, min=1, max=5100, scale=1., unit="Hz"))
-        self.setattr_argument("time_total_s",       NumberValue(default=1, precision=0, step=1, min=1, max=100000, scale=1., unit="s"))
+        self.setattr_argument("sample_rate_hz", NumberValue(default=5000, precision=3, step=1, min=1, max=5100, scale=1., unit="Hz"),
+                              tooltip="The ADC sample rate.\n"
+                                      "This is limited to 5kHz for long-term measurement.\n"
+                                      "For shorter timescales (e.g. ~1s), a sample rate of up to 20kHz can be achieved.")
+        self.setattr_argument("time_total_s",   NumberValue(default=1, precision=0, step=1, min=1, max=100000, scale=1., unit="s")
+                              tooltip=("The total length of time to record for.\n"
+                                       "Note: once this experiment begins, it can not be cancelled without loss of data, "
+                                       "and termination CANNOT be requested.\n"
+                                       "If possible, a long run should be broken up into multiple smaller runs.")
 
     def prepare(self):
         # general
@@ -68,10 +82,16 @@ class SamplerRead(EnvExperiment):
                     self.adc.sample_mu(sampler_buffer)
                     self.update_dataset(i, sampler_buffer)
 
+        # ensure events finish submission before experiment completion
+        self.core.break_realtime()
+        self.core.wait_until_mu(now_mu())
+
     @rpc(flags={"async"})
     def update_dataset(self, i, volts_mu_arr):
         """
         Records values via rpc to minimize kernel overhead.
+        :param i: todo: document
+        :param volts_mu_arr: todo: document
         """
         data = np.array(volts_mu_arr)[self.channel_list] * self.adc_mu_to_v_list
         self.mutate_dataset("results", i, data)
