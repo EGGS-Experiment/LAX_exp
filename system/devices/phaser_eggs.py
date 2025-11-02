42f9cbb4139ca237102988e56501994fb2914d5f
EGGS_lab <hudsonlabwifiweather1@gmail.com>
2025-11-02 14:29:03 -0800

    saving changes 2025_11_02

--- system/devices/phaser_eggs.py
+++ system/devices/phaser_eggs.py
@@ -48,6 +48,7 @@ class PhaserEGGS(LAXDevice):
         # ensure delays are multiples of phaser frame period
         self.time_phaser_holdoff_mu = int64(round(self.time_phaser_holdoff_mu / self.t_frame_mu) * self.t_frame_mu)
 
+        # note: make all delays self.core.coarse_ref_period instead of 8ns
         self.switch_delay_time_mu = int64(8)
 
     @kernel(flags={'fast-math'})
@@ -238,7 +239,6 @@ class PhaserEGGS(LAXDevice):
         # add delay for oscillator updates to account for pipeline latency
         delay_mu(2560)  # 8 frame periods
         # stop phaser amp switches & deactivate integrator hold
-
         self.amp_sws_off()
 
         # add delay time after EGGS pulse to allow RF servo to re-lock
